2d0da56d63c399936609ca7ab8c4dabab1b78b48
Denis Boogaerts Afelio <d.boogaerts@afelio.be>
2020-04-24 11:39:24 +0200

    support

--- live/24042020/live.js
+++ live/24042020/live.js
@@ -1,1 +1,13 @@
-console.log('hello');
+// propriété
+console.log('hello', document.querySelector('.container span').textContent);
+window.document.querySelector('.container span').textContent = "Salut";
+
+//méthode
+window.alert('coucou')
+
+//évènement
+document.querySelector('.container span').addEventListener('click', () => 
+{
+    console.log('click');
+    
+})
