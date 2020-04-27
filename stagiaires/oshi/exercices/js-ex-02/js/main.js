08c57ade64e2eb5694f9a17f7c543a87ee28ec19
BlackScaryZ <blackscaryz@gmail.com>
2020-04-27 17:33:02 +0200

    Ajout des exos

---
+++ stagiaires/oshi/exercices/js-ex-02/js/main.js
@@ -0,0 +1,18 @@
+function coupure(number) {
+    number = Math.floor(number * 100);
+    const coupures = [50000,20000,10000,5000,2000,1000,500,200,100,50,20,10,5,2,1];
+    const coupures_result = [];
+    coupures.forEach((coupure, i) => {
+      coupures_result.push(Math.floor(number / coupure));
+      number = number - coupures_result[coupures_result.length-1] * coupure;
+      console.log(number);
+    });
+    return coupures_result;
+}
+document.getElementById('inumber').addEventListener('change', () => {
+  coupure(document.getElementById('inumber').value);
+  const results = [...document.getElementsByClassName("result")];
+  results.forEach((element, c) => {
+    element.innerHTML = coupure(document.getElementById('inumber').value)[c];
+  });
+});
