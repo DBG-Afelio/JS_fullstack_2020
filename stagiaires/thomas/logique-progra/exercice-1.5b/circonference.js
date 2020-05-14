55c10ed9c9028d84dd011437d81b921d9761d49e
Thomas <thomaskouadio92@gmail.com>
2020-05-14 13:32:49 +0200

    test insert svg

--- stagiaires/thomas/logique-progra/exercice-1.5b/circonference.js
+++ stagiaires/thomas/logique-progra/exercice-1.5b/circonference.js
@@ -1,12 +1,24 @@
 
 // VARIABLES
-const canvas = document.querySelector('#canvas');
+const playground = document.querySelector('#playground');
 const svg = document.querySelector("#circle-svg");
+const circle = document.createElement("circle");
 
 
 // PROGRAMME
-canvas.style.width = "600px";
-canvas.style.height = "600px";
+playground.style.width = "900px";
+playground.style.height = "600px";
 svg.style.width = "600px";
 svg.style.height = "600px";
+
+
+circle.setAttribute("x","0");
+circle.setAttribute("y","0");
+circle.setAttribute("cx","50");
+circle.setAttribute("cy","50");
+circle.setAttribute("r","50");
+circle.style.fill = "green";
+svg.appendChild(circle);
+
+
     
