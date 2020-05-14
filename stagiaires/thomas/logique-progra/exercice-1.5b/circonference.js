afc4676eaf34607303fa7be6f2a898e746306940
Thomas <thomaskouadio92@gmail.com>
2020-05-14 11:33:22 +0200

    add starter files

---
+++ stagiaires/thomas/logique-progra/exercice-1.5b/circonference.js
@@ -0,0 +1,12 @@
+
+// VARIABLES
+const canvas = document.querySelector('#canvas');
+const svg = document.querySelector("#circle-svg");
+
+
+// PROGRAMME
+canvas.style.width = "600px";
+canvas.style.height = "600px";
+svg.style.width = "600px";
+svg.style.height = "600px";
+    
