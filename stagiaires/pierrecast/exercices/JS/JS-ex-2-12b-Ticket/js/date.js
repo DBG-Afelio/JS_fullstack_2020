0a884b4b65b65b65c6203082c72555eb6258491d
pierre-cast <p_castronovo@hotmail.com>
2020-05-14 13:34:43 +0200

    PC-Logique Ticket JS et CSS (sans AlertBox)

--- stagiaires/pierrecast/exercices/JS/JS-ex-2-12b-Ticket/js/date.js
+++ stagiaires/pierrecast/exercices/JS/JS-ex-2-12b-Ticket/js/date.js
@@ -1,9 +1,14 @@
 const today = new Date();
+const oResult = document.querySelector('#result');
+const oSubmit = document.querySelector('#submit');
+const oDate = document.querySelector('#date');
 
-document.querySelector('#submit').addEventListener('click', function (e) {
+oSubmit.addEventListener('click', function (e) {
     e.preventDefault;
     submit();
 });
+
+//oDate.addEventListener('focus', clearResult);
 
 function submit() {
     let birthDate = document.querySelector('#date').value; console.log('birthDate : ', birthDate);
@@ -16,13 +21,13 @@ function submit() {
         console.log("birthDate : ", birthDate, "age : ", transformAge(age), "ticketType : ", ticketType);
        
         if (ticketType === 'ticketA' || (ticketType === 'ticketB' && age < 12) || age < 6) {
-            alert ('Vous pouvez entrer. Bonne Visite !');
+            insertResult('Vous pouvez entrer. Bonne Visite !', true);
         } else {
             if (age >= 6) {
                 if (age >= 12) {
-                    alert ('Vous avez 12 ans ou plus ('+transformAge(age)+'). Il vous faut un ticket A !');
+                    insertResult('Vous avez 12 ans ou plus ('+transformAge(age)+'). Il vous faut un ticket A !', false);
                 } else {
-                    alert ('Vous avez 6 ans ou plus ('+transformAge(age)+'). Il vous faut un ticket B');
+                    insertResult('Vous avez 6 ans ou plus ('+transformAge(age)+'). Il vous faut un ticket B', false);
                 }
             } 
         }
@@ -63,6 +68,29 @@ function submit() {
         //si l'année est divisible par 4 et non divisible par 100 ;
         //si l'année est divisible par 400.
 
-        return year % 400 === 0 || (year  % 4 === 0 && year % 100 !== 0);
+        return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+    }
+
+    function insertResult(txt, enter) {
+        /*let oDisc = document.createElement('div');
+        let oP = document.createElement('p')
+        if (enter) {
+            let oCheck = document.createTextNode('V');
+            oDisc.setAttribute('class', 'good')
+        } else {
+            let oCheck = document.createTextNode('X');
+            oDisc.setAttribute('class', 'wrong')
+        }
+        oResult.appendChild(oDisc)
+        
+        oP.appendChild(document.createTextNode(txt));
+        oResult.appendChild(oP);*/
+
+        alert(txt);
+
+    }
+
+    function clearResult() {
+        oResult.innerHTML = '';
     }
 }
