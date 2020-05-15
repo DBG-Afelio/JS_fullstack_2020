4bd328efbc8b4c7343bec553e11032c087b752e9
Thomas <thomaskouadio92@gmail.com>
2020-05-15 11:21:31 +0200

    avancee circonf.

--- stagiaires/thomas/logique-progra/exercice-1.5b/circonference.js
+++ stagiaires/thomas/logique-progra/exercice-1.5b/circonference.js
@@ -1,24 +1,47 @@
-
 // VARIABLES
-const playground = document.querySelector('#playground');
-const svg = document.querySelector("#circle-svg");
-const circle = document.createElement("circle");
+let radiusEntry;
+const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+const svgSandbox = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+const validateRadius = document.querySelector(".validate-radius");
+let redValue = Number(document.querySelector("input[name='r-range']").value);
+let greenValue = Number(document.querySelector("input[name='g-range']").value);
+let blueValue = Number(document.querySelector("input[name='b-range']").value);
+let alphaValue = Number(document.querySelector("input[name='a-range']").value);
 
 
-// PROGRAMME
-playground.style.width = "900px";
-playground.style.height = "600px";
-svg.style.width = "600px";
-svg.style.height = "600px";
+// START 
+
+// redValue = 0;
+// greenValue = 0;
+// blueValue = 0;
+// alphaValue = 0;
 
 
-circle.setAttribute("x","0");
-circle.setAttribute("y","0");
-circle.setAttribute("cx","50");
-circle.setAttribute("cy","50");
-circle.setAttribute("r","50");
-circle.style.fill = "green";
-svg.appendChild(circle);
+//SVG SANDBOX
+document.body.append(svgSandbox);
+svgSandbox.classList.add("svg-sandbox");
+
+// ----
+svgSandbox.append(circle);
+circle.setAttribute("cx", "50");
+circle.setAttribute("cy", "50");
+circle.setAttribute("r", "50");
+circle.style.fill = "gray";
+circle.style.strokeWidth = "5px";
+circle.style.stroke = "black";
 
 
+let arrayColor
+
+validateRadius.addEventListener("click", function() {
+
+ radiusEntry = Number(document.querySelector(".radius-entry").value)
+ console.log(radiusEntry);
+
+})
+
+
+
+
+console.log(greenValue)
     
