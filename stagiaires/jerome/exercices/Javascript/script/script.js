2065843a77b5a9b0c40efb1315aa4d3bbfa2f1bb
Jer1990 <jerome.grand@hotmail.be>
2020-05-15 11:25:23 +0200

    exe ticket presque fin

--- stagiaires/jerome/exercices/Javascript/script/script.js
+++ stagiaires/jerome/exercices/Javascript/script/script.js
@@ -4,14 +4,42 @@ function getAge(date) {
         return Math.abs(age.getUTCFullYear() - 1970);
     }
 
-
-let year;
-let month;
-let day;
 const validation=document.getElementById("validation");
+let radio=document.querySelectorAll("input[type=radio]");
 validation.addEventListener("click",()=>{
+
 	let dateSelect=document.querySelector("input[type=date]");
-	console.log(dateSelect.value)
-	getAge(dateSelect.value)
+	let ageEnter =getAge(dateSelect.valueAsDate)
+
+	if(ageEnter<0 || ageEnter>130){
+
+		alert("Entrez une date valide")
+
+	}else{
+
+		for (let element of radio){
+			if(element.checked){
+				switch (element.value){
+					case '1':if(ageEnter>6){
+								alert("Ticket Invalide");
+							}else alert("Ticket valide;");
+					break;
+
+					case '2':if(ageEnter>12 || ageEnter<=6){
+								alert("Ticket Invalide");
+							}else alert("Ticket valide; Prix= 250 euro");
+					break;
+
+					case '3':if(ageEnter<=12){
+								alert("Ticket Invalide");
+							}else alert("Ticket valide; Prix= 1500 euro");
+					break;
+
+				}
+			}
+		} 
+
+		
+	}
 
 })
