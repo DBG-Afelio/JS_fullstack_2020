9b322b639b4591b741f8b4407ebdff02da864c8e
Fripon08 <62310112+Fripon08@users.noreply.github.com>
2020-05-15 13:09:12 +0200

    Nouvelle version

--- stagiaires/florent/exercices/Logique/Exo 1.12b - Ticket/ticket.js
+++ stagiaires/florent/exercices/Logique/Exo 1.12b - Ticket/ticket.js
@@ -1,32 +1,53 @@
-var aujourdhui = new Date();
+let aujourdhui = new Date();
 const TEXT_VALIDE = "Vous êtes en ordre. Bienvenu!";
 const TEXT_NONVALIDE = "Vous n'êtes pas en ordre. Veuillez vous procurez un ticket valide ou dire la vérité sur votr âge. Merci!";
-var choix1 = document.getElementById("normal").value;
-var choix2 = document.getElementById("reduit").value;
-var choix3 = document.getElementById("aucun").value;
+const choix1 = document.getElementById("normal").value;
+const choix2 = document.getElementById("reduit").value;
+const choix3 = document.getElementById("aucun").value;
 
 document.getElementById('sub').addEventListener('click', execute);
 
 function execute(){
-    const date = document.getElementById('age').value;
-    if (date != aujourdhui.toLocaleString()) {
-        var valeur = document.querySelector('input[name=choix]:checked').value;
-        if (valeur == choix1 && date) {
-            document.getElementById('reponse').value = "Normal"
+    const date = document.getElementById('age').valueAsDate;
+    if (date < aujourdhui) {
+        const valeur = document.querySelector('input[name=choix]:checked').value;
+        const age = getAge(date, aujourdhui);
+        if (valeur === choix1 && age >= 12) {
+            document.getElementById('reponse').value = "Normal";
         }
-        if (valeur == choix2 && date) {
-            document.getElementById('reponse').value = "Reduit"
+        if (valeur === choix2 && age < 12 && age>=6) {
+            document.getElementById('reponse').value = "Reduit";
         }
-        if (valeur == choix3 && date) {
-            document.getElementById('reponse').value = "Aucun"
+        if (valeur === choix3 && age<6) {
+            document.getElementById('reponse').value = "Aucun";
         }
-        console.log(aujourdhui.toLocaleString());
-        console.log(getAge(date));
+        document.getElementById('datevalide').value = "";
+        console.log(aujourdhui);
+        console.log(getAge(date, aujourdhui));
+    } else {
+        document.getElementById('datevalide').value = "Date non valide!";
     }
 }
 
-function getAge(date) { 
-    var diff = Date.now() - date.getTime();
-    var age =  date(diff); 
-    return Math.abs(age.getUTCFullYear() - 1970);
+function getAge(date, aujourdhui) {  
+    var jourdate = date.getDate();
+    var jourmois = date.getMonth();
+    var jouran = date.getFullYear();
+    var jouraujourdhui = aujourdhui.getDate();
+    var moisaujourdhui = aujourdhui.getMonth();
+    var anaujourdhui = aujourdhui.getFullYear();
+    var agesans = anaujourdhui - jouran;
+    var agesmois = moisaujourdhui - jourmois;
+
+    if (agesmois <0) {
+        agesans = agesans + 1;
+    } else {
+        if (agesmois == 0){
+            var agesjour = jouraujourdhui - jourdate;
+            if (agesjour < 0){
+                agesans = agesans - 1; 
+            }
+        }
+    }
+    return agesans;
 }
