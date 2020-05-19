a7561fafc635cfa64ccc02eb120f1a426eb49fbf
zaidun <35617206+zaidun@users.noreply.github.com>
2020-05-19 13:19:46 +0200

    js

--- stagiaires/haroon/exercices/login/js/login.js
+++ stagiaires/haroon/exercices/login/js/login.js
@@ -11,49 +11,57 @@ function validateInput() {
 }
 
 button.addEventListener('click',()=>{
-    let haroon = document.getElementById("lastName").value;
+    let haroon = document.getElementById("lastName").Value;
     validateHaroon(haroon);
 })
 
 
 function validateHaroon(inputedValue){
-    inputedValue= document.getElementById('lastName').value;
+    console.log(inputedValue);
+    const inputedValueInput = inputedValue.value;
+    console.log(inputedValueInput);
     let trueInput = true ; 
-    inputedValue.classlist.remove('valide');
-    inputedValue.classlist.remove('invalide');
-    if (inputedValue.trim()===''){
+    inputedValue.classList.remove('valide');
+    inputedValue.classList.remove('invalide');
+    if (inputedValueInput.trim()===''){
         trueInput = false ; 
-        inputedValue.classlist.add('valide');
-        inputedValue.classlist.add('error-required');
+        inputedValue.classList.add('valide');
+        inputedValue.classList.add('error-required');
     
     }
     else{
-    falseTrue =validetNom(inputedValue);
-        if (falseTrue === 1){
+    const valid = validetNom(value);
+        if (valid === 1){
             alert ("name is longer than normal" );
             inputedValue.classList.add('invalide');
-            inputedValue.classList.add('error-min-length');
+            inputedValue.classList.add('error-max-length');
             falseTrue = false;
         }
-        else if  (falseTrue === 2){
+        else if  (valid === 2){
             alert ("name is shorter than normal");
+            inputedValue.classList.add('invalide');
+            inputedValue.classList.add('error-max-length');
+            falseTrue = false;
         }
         else {
             alert ("go ahead ");
+            inputedValue.classList.add('valide');
         }
-        }
+    }
+        return falseTrue ;
+        
+    }
         function validetNom(name){
         
-        
             if (name.length > 5 ){
-                return 1 ;
+                return 0 ;
             }
             else if  (name.length < 3 ){
-                return 2;
+                return 1;
             }
         else {
-            alert 
-            return 3;
+             
+            return 2;
         
         }
     }
