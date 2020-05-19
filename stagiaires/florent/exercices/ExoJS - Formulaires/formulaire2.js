33849969969c70e3cbd155d822d1b93d7fdd9adf
Fripon08 <62310112+Fripon08@users.noreply.github.com>
2020-05-19 11:18:45 +0200

    MAJ

--- stagiaires/florent/exercices/ExoJS - Formulaires/formulaire2.js
+++ stagiaires/florent/exercices/ExoJS - Formulaires/formulaire2.js
@@ -1,12 +1,20 @@
-let NOM = document.getElementById('nom').value;
-let PRENOM = document.getElementById('prenom').value;
 
 document.getElementById('sub').addEventListener('click', execute);
 
 
 function execute (){
-   document.getElementById('repnom').value = nom();
-   document.getElementById('repprenom').value = prenom();
+    let NOM = document.getElementById('nom').value;
+    let PRENOM = document.getElementById('prenom').value;
+    let MAIL = document.getElementById('mail').value;
+    const DATE = document.getElementById('date').valueAsDate;
+    let LOGIN = document.getElementById('login').value;
+    let MP1 = document.getElementById('MP').value;
+    let MPC = document.getElementById('MPC').value;
+
+   document.getElementById('repnom').value = nom(NOM);
+   document.getElementById('repprenom').value = prenom(PRENOM);
+   document.getElementById('repmail').value = mail(MAIL);
+   document.getElementById('repdate').value = date(DATE);
 }
 
 function nom(NOM){
@@ -36,4 +44,42 @@ function nom(PRENOM){
             return "";
         }
     }
+}
+
+function date(DATE){
+    let aujourdhui = new Date();
+    let age = getAge(DATE, aujourdhui);
+
+    if (age < 18) {
+        return "Votre être trop jeune";
+    } else {
+        if (age > 67) {
+            return "Vous avez dépassé l'âge maximun";
+        } else {
+            return "";
+        }
+    }
+}
+
+function getAge(DATE, aujourdhui) {  
+    var jourdate = DATE.getDate();
+    var jourmois = DATE.getMonth();
+    var jouran = DATE.getFullYear();
+    var jouraujourdhui = aujourdhui.getDate();
+    var moisaujourdhui = aujourdhui.getMonth();
+    var anaujourdhui = aujourdhui.getFullYear();
+    var agesans = anaujourdhui - jouran;
+    var agesmois = moisaujourdhui - jourmois;
+
+    if (agesmois <0) {
+        agesans = agesans - 1;
+    } else {
+        if (agesmois == 0){
+            var agesjour = jouraujourdhui - jourdate;
+            if (agesjour < 0){
+                agesans = agesans - 1; 
+            }
+        }
+    }
+    return agesans;
 }
