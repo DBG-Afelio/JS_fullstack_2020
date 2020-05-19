31dab88fd889a032698d63f1a09eb00514ce858d
Fripon08 <62310112+Fripon08@users.noreply.github.com>
2020-05-19 13:28:36 +0200

    Update

--- stagiaires/florent/exercices/ExoJS - Formulaires/formulaire2.js
+++ stagiaires/florent/exercices/ExoJS - Formulaires/formulaire2.js
@@ -1,5 +1,6 @@
 
 document.getElementById('sub').addEventListener('click', execute);
+document.getElementById('sugg').addEventListener('click', suggestionlogin);
 
 
 function execute (){
@@ -8,13 +9,16 @@ function execute (){
     let MAIL = document.getElementById('mail').value;
     const DATE = document.getElementById('date').valueAsDate;
     let LOGIN = document.getElementById('login').value;
-    let MP1 = document.getElementById('MP').value;
-    let MPC = document.getElementById('MPC').value;
+    let MP1 = document.getElementById('mp').value;
+    let MPC = document.getElementById('mpc').value;
 
    document.getElementById('repnom').value = nom(NOM);
    document.getElementById('repprenom').value = prenom(PRENOM);
    document.getElementById('repmail').value = mail(MAIL);
    document.getElementById('repdate').value = date(DATE);
+   document.getElementById('replogin').value = login(LOGIN);
+   document.getElementById('repMP').value = mp(MP1);
+   document.getElementById('repMPC').value = mpc(MPC, MP1);
 }
 
 function nom(NOM){
@@ -82,4 +86,56 @@ function getAge(DATE, aujourdhui) {
         }
     }
     return agesans;
+}
+
+function mail(MAIL){
+    const carac = '@';
+    if (MAIL.includes(carac) == false ) {
+        return "L'email ne respecte pas le format";
+    } else {
+        return "";
+    }
+}
+
+function login(LOGIN){
+    if (LOGIN.length < 6 || LOGIN.length >10) {
+        return "Le mot de passe doit contenir entre 6 et 10 caractères";
+    } else {
+        return "";
+    }
+}
+
+function suggestionlogin (){
+    let sugnom = document.getElementById('nom').value;
+    let sugprenom = document.getElementById('prenom').value;
+
+    let newnom = sugnom.slice(0, 2);
+    let newprenom = sugprenom.slice(0, 1);
+
+    let newlogin = (newprenom+newnom+"_"+"123");
+
+    document.getElementById('login').value = newlogin;
+}
+
+function mp(MP1){
+    if (MP1.length == 0) {
+        return "Veuillez complèter ce champ merci";
+    } else {
+        if (MP1.length < 6) {
+            return "MP faible";
+        } else {
+            
+            if (MP1.length > 6 && MP1.length < 10) {
+                return "MP moyen";
+            } else {
+                return "MP fort";
+            }
+        }
+    }
+}
+
+function mpc(MPC, MP1){
+    if (MPC != MP1){
+        return "Les mots de passe de correspondent pas!";
+    }
 }
