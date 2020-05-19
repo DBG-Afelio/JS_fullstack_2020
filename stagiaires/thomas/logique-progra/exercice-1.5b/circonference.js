8d6659531bab1b8e51597f56f1a9a5fdf40c6e3a
Thomas <thomaskouadio92@gmail.com>
2020-05-19 10:16:45 +0200

    add colors fill / formula

--- stagiaires/thomas/logique-progra/exercice-1.5b/circonference.js
+++ stagiaires/thomas/logique-progra/exercice-1.5b/circonference.js
@@ -1,21 +1,15 @@
 // VARIABLES
-let radiusEntry;
 const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 const svgSandbox = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 const validateRadius = document.querySelector(".validate-radius");
-let redValue = document.querySelector("input[name='r-range']");
-let greenValue = document.querySelector("input[name='g-range']");
-let blueValue = document.querySelector("input[name='b-range']");
-let alphaValue = document.querySelector("input[name='a-range']");
+const redRange = document.querySelector("input[name='r-range']");
+const greenRange = document.querySelector("input[name='g-range']");
+const blueRange = document.querySelector("input[name='b-range']");
+const alphaRange = document.querySelector("input[name='a-range']");
+const arrayOfRanges = [redRange, greenRange, blueRange, alphaRange];
 
 
 // START 
-
-// redValue = 0;
-// greenValue = 0;
-// blueValue = 0;
-// alphaValue = 0;
-
 
 //SVG SANDBOX
 document.body.append(svgSandbox);
@@ -26,30 +20,22 @@ svgSandbox.append(circle);
 circle.setAttribute("cx", "50");
 circle.setAttribute("cy", "50");
 circle.setAttribute("r", "50");
-circle.style.strokeWidth = "5px";
+circle.style.strokeWidth = "2px";
 circle.style.stroke = "black";
 
-
-let arrayColor = [redValue, greenValue, blueValue, alphaValue]
-
-arrayColor.forEach((range) => {
+arrayOfRanges.forEach((range) => {
 
     range.addEventListener("change", function() {
-        console.log(range.value);
-        circle.setAttribute("fill", "rgba: ");
+        circle.setAttribute("fill", "rgba(" + redRange.value + ", " + greenRange.value + ", " + blueRange.value + ", " + "0." + alphaRange.value + ")");
     })
 
 })
 
 validateRadius.addEventListener("click", function() {
-
- radiusEntry = Number(document.querySelector(".radius-entry").value)
- console.log(radiusEntry);
+    const radiusEntry = Number(document.querySelector(".radius-entry").value)
+    const circonference = Number((2 * Math.PI * radiusEntry).toFixed(2))
+    console.log(circonference);
 
 })
 
-
-
-
-// console.log(greenValue)
     
