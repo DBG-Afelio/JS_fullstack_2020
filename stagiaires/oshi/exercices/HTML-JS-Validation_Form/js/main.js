18451c2a76f93986bb0d875b3e0135989f7ac60c
BlackScaryZ <blackscaryz@gmail.com>
2020-05-19 08:44:33 +0200

    Etat actuelle

--- stagiaires/oshi/exercices/HTML-JS-Validation_Form/js/main.js
+++ stagiaires/oshi/exercices/HTML-JS-Validation_Form/js/main.js
@@ -2,23 +2,28 @@ document.forms[0].addEventListener('submit', e => {
     e.preventDefault();
 });
 document.forms[0].addEventListener('input', e => {
-    checkLength(e.target)
+    console.log(e);
+    checkElement(e.target);
 })
 
-function checkLength(element) {
+function checkElement(element) {
+    //Vérification reguired
+    if(element.required && element.value.trim() === "") {
+        element.parentElement.querySelector('.required').style.display = "block";
+    } else if(element.required) {
+        element.parentElement.querySelector('.required').style.display = "none";
+    }
+    //Vérification longueur
     if(element.maxLength > 0 && element.minLength > 0) {
-        if(element.maxLength < element.value.length || element.minLength > element.value.length) {
-            element.parentElement.querySelector('.no-valid').style.display = "block";
-            element.parentElement.querySelector('.valid').style.display = "none";
+        if(element.maxLength < element.value.length || element.minLength > element.value.length && element.value.trim() !== "") {
             if(element.maxLength < element.value.length) {
-
+                element.parentElement.querySelector('.max-length-no-valid').style.display = "block";
             } else {
                 element.parentElement.querySelector('.min-length-no-valid').style.display = "block";
             }
         } else {
-            element.parentElement.querySelector('.valid').style.display = "block";
-            element.parentElement.querySelector('.no-valid').style.display = "none";
             element.parentElement.querySelector('.min-length-no-valid').style.display = "none";
+            element.parentElement.querySelector('.max-length-no-valid').style.display = "none";
         }
     }
 }
