44671cb0797ce6791720cd41ea96f6e9decc9310
BlackScaryZ <blackscaryz@gmail.com>
2020-05-19 11:12:30 +0200

    A jour

--- stagiaires/oshi/exercices/HTML-JS-Validation_Form/js/main.js
+++ stagiaires/oshi/exercices/HTML-JS-Validation_Form/js/main.js
@@ -1,3 +1,5 @@
+const now = new Date();
+
 document.forms[0].addEventListener('submit', e => {
     e.preventDefault();
 });
@@ -7,16 +9,17 @@ document.forms[0].addEventListener('input', e => {
 })
 
 function checkElement(element) {
+    const value = element.value.trim();
     //Vérification reguired
-    if(element.required && element.value.trim() === "") {
+    if(element.required && value === "") {
         element.parentElement.querySelector('.required').style.display = "block";
     } else if(element.required) {
         element.parentElement.querySelector('.required').style.display = "none";
     }
     //Vérification longueur
     if(element.maxLength > 0 && element.minLength > 0) {
-        if(element.maxLength < element.value.length || element.minLength > element.value.length && element.value.trim() !== "") {
-            if(element.maxLength < element.value.length) {
+        if(element.maxLength < value.length || element.minLength > value.length && value !== "") {
+            if(element.maxLength < value.length) {
                 element.parentElement.querySelector('.max-length-no-valid').style.display = "block";
             } else {
                 element.parentElement.querySelector('.min-length-no-valid').style.display = "block";
@@ -26,4 +29,39 @@ function checkElement(element) {
             element.parentElement.querySelector('.max-length-no-valid').style.display = "none";
         }
     }
+    //Vérification custom
+    if(element.hasAttribute('c-function') && window[element.getAttribute('c-function')]) {
+        const valid = window[element.getAttribute('c-function')](element);
+        if(!valid && value !== "") {
+            element.parentElement.querySelector('.custom-error').style.display = "block";
+        } else {
+            element.parentElement.querySelector('.custom-error').style.display = "none";
+        }
+    }
+}
+
+function check_email(element) {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(element.value).toLowerCase());
+}
+
+function check_tel(element) {
+    const regex = /^(0|\+32|0032)[1-9](\d){6,9}$/;
+    return regex.test(String(element.value));
+}
+
+function check_birthday(element) {
+    const birth = new Date (element.value);
+    const diff = now.getFullYear() - birth.getFullYear();
+    if(diff >= 18 && diff <= 67) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+function setMinMaxBirthDay() {
+
 }
