bf8a28c1bc55a0a76a2e2daf311846e9ab8deb70
Thomas <thomaskouadio92@gmail.com>
2020-05-22 00:30:55 +0200

    Starter

---
+++ stagiaires/thomas/exercices/validation-form-full/form.js
@@ -0,0 +1,13 @@
+// CHAMPS HTML
+const nom = document.querySelector(".nom");
+const prenom = document.querySelector(".prenom");
+const email = document.querySelector(".email");
+const telephone = ducument.querySelector(".telephone");
+const dateDeNaissance = document.querySelector(".date-de-naissance");
+const login = document.querySelector(".login");
+const suggestLogin = document.querySelector(".suggest-login");
+const password = document.querySelector(".mot-de-passe");
+const confirmPassword = document.querySelector(".confirm-mot-de-passe");
+const passTabs = document.querySelectorAll(".force-passe-cntnr"); 
+const country = document.querySelector(".country");
+const radiosGender = document.querySelectorAll("input[name='gender']");
