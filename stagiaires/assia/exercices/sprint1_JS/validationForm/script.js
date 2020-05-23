05b43e4acbc2783e7bd6e0aeccb11e9a3a50ccb2
AssiaRachdi <arbox00@outlook.com>
2020-05-23 15:32:21 +0200

    -

--- stagiaires/assia/exercices/sprint1_JS/validationForm/script.js
+++ stagiaires/assia/exercices/sprint1_JS/validationForm/script.js
@@ -13,13 +13,10 @@ const fieldPwdConfirmed = document.querySelector('.form-mdp-confirm');
 const fieldNationality = document.querySelector('.form-nationality');
 const fieldSex = document.querySelector('.form-sex');
 const sexRadioAutre = document.querySelector("#autre");
+const myForm = document.querySelector('#myform');
 
 sexRadioAutre.checked = true;
 
-fieldSubmit.addEventListener('click', (e)=> {
-    validate(fieldSubmit); 
-    e.preventDefault();
-});
 fieldName.addEventListener('input', () => {
     validateFieldName(fieldName);
 });
@@ -53,8 +50,15 @@ fieldNationality.addEventListener('change', () => {
 fieldSex.addEventListener('input', () => {
     validateFieldSex(fieldSex,sexRadioAutre);
 });
-
-//-----------------------------------------
+/*
+myForm.addEventListener('change', () => {
+    validate(fieldSubmit); 
+});
+*/
+fieldSubmit.addEventListener('click', (e)=> {
+    validate(fieldSubmit); 
+    e.preventDefault();
+});
 /*function isFormValid() {
     let stateOfAllInputField = [validateFieldName, validateFieldDate];
     console.log(stateOfAllInputField);
@@ -63,36 +67,37 @@ fieldSex.addEventListener('input', () => {
         return !isAllValid;
     });
 }*/
-
+//-----------------------------------------
 /**
  * Fonction validant tous les champs
  * 
  */
+
+
 function validate(submitBtn) {
-    submitBtn.classList.remove('valide');
-    submitBtn.classList.remove('invalide');
-    if (!validateFieldName(fieldName) || !validateFieldDate(fieldDate) || !validateFieldFirstName(fieldFirstName)){
-        submitBtn.classList.add('invalide');
+    let allFieldsStatus = [validateFieldName(fieldName), validateFieldFirstName(fieldFirstName), validateFieldTel(fieldTel), validateFieldEmail(fieldEmail), validateFieldDate(fieldDate), validateFieldLogin(fieldLogin), validateFieldPassword(fieldPwd), validateFieldPasswordConfirmation(fieldPwdConfirmed), validateFieldNationality(fieldNationality), validateFieldSex(fieldSex)];
+    console.log(allFieldsStatus);
+    let isNotValid = allFieldsStatus.includes(false);
+    console.log(isNotValid);
+    submitBtn.classList.remove('invalide-form', 'valide-form');
+    if (isNotValid) {
+        submitBtn.classList.add('invalide-form');
     }
     else {
-        submitBtn.classList.add('valide');
+        submitBtn.classList.add('valide-form');
     }
 }
 
+
 function removeClassLists(element) {
-    element.classList.remove('valide');
-    element.classList.remove('invalide');
-    element.classList.remove('error-message');
+    element.classList.remove('valide', 'invalide', 'error-required', 'error-min-login', 'error-max-login', 'error-login-used', 'error-pattern', 'error-tel', 'error-different-pwd', 'error-email', 'error-min-length', 'error-max-length', 'error-dateIsFutur', 'error-ageIsOut');
 }
 function validateFieldSex(sexField, optionAutre) {
     const isAutreChecked = optionAutre.checked;
     let validReturn = true;
-    sexField.classList.remove('valide');
-    sexField.classList.remove('invalide');
-    sexField.classList.remove('error-required');
+    removeClassLists(sexField);
     if (isAutreChecked) {
-        sexField.classList.add('invalide');
-        sexField.classList.add('error-required');
+        sexField.classList.add('invalide', 'error-required');
         validReturn = false;
     } else {
         sexField.classList.add('valide');
@@ -103,19 +108,15 @@ function validateFieldNationality(nationalityField) {
     const nationSelectField = nationalityField.querySelector('select');
     const nationSelected = nationSelectField.value; 
     const validReturn = true;
-    nationalityField.classList.remove('valide');
-    nationalityField.classList.remove('invalide');
-    nationalityField.classList.remove('error-required');
+    removeClassLists(nationalityField);
     if (nationSelected === 'nothingSelected') {
-        nationalityField.classList.add('invalide');
-        nationalityField.classList.add('error-required');
+        nationalityField.classList.add('invalide', 'error-required');
         validReturn = false;
     } else {
         nationalityField.classList.add('valide');
     }
     return validReturn;
 }
-
 /**
  * fonction qui valide et met a jour le champ TEL
  * @param 
@@ -124,24 +125,19 @@ function validateFieldNationality(nationalityField) {
 function validateFieldTel(telField) {
     const telInputField = telField.querySelector('input');
     const telValue = telInputField.value;
-    console.log('telValue : ' + telValue);
-    telField.classList.remove('valide');
-    telField.classList.remove('invalide');
-    telField.classList.remove('error-required');
-    telField.classList.remove('error-tel');
+    removeClassLists(telField);
     let validReturn = true;
-    if (telValue.trim == '') {
-        telField.classList.add('invalide');
-        telField.classList.add('error-required');
+    if (telValue.trim() == '') {
+        telField.classList.add('invalide', 'error-required');
         validReturn = false;
     } else {
-        const isValid = isTelValid(telValue); console.log('isValid : ' + isValid);
+        let isValid = isTelValid(telValue);
+                                                console.log('isValid : ' + isValid);
         // BUG :why isValid = false when isTelValid() returns true ????
         if (isValid) {
             telField.classList.add('valide');
         } else {
-            telField.classList.add('invalide');
-            telField.classList.add('error-tel');
+            telField.classList.add('invalide', 'error-tel');
             validReturn = false;
         }
     }
@@ -154,8 +150,8 @@ function validateFieldTel(telField) {
  */
 function isTelValid(telNumber) {
     const patternTel = new RegExp(/(?:(^([+32]{3}|[0032]{4}|[0])([1-9]{6,9})$))/g);
-    console.log('return pattern tel : ' + (patternTel.test(telNumber)));
-    return patternTel.test(telNumber);
+    console.log('is Tel Pattern right ? : ' + (patternTel.test(telNumber)));
+    return (patternTel.test(telNumber));
 }
 
 function suggestLogin(firstNameField, lastNameField, loginField) {
@@ -178,22 +174,14 @@ function validateFieldLogin(loginField) {
     const loginInputField = loginField.querySelector('input');
     const loginValue = loginInputField.value;
     const hasLoginBeenUsed = loginUsedList.includes(loginValue);
-    loginField.classList.remove('valide');
-    loginField.classList.remove('invalide');
-    loginField.classList.remove('error-required');
-    loginField.classList.remove('error-min-login');
-    loginField.classList.remove('error-max-login');
-    loginField.classList.remove('error-login-used');
-    loginField.classList.remove('error-pattern');
+    removeClassLists(loginField);
     let validReturn = true;
     console.log('methode includes : '+hasLoginBeenUsed);
     if (loginValue.trim() === '') {
-        loginField.classList.add('invalide');
-        loginField.classList.add('error-required');
+        loginField.classList.add('invalide', 'error-required');
         validReturn = false;
     } else if (hasLoginBeenUsed) {
-        loginField.classList.add('invalide');
-        loginField.classList.add('error-login-used');
+        loginField.classList.add('invalide', 'error-login-used');
         validReturn = false;
     } else {
         const loginStatus = isContentValid(loginValue); console.log('Content CASE : ' + loginStatus);
@@ -202,30 +190,23 @@ function validateFieldLogin(loginField) {
                 loginField.classList.add('valide');
                 break;
             case 1:
-                loginField.classList.add('invalide');
-                loginField.classList.add('error-pattern');
+                loginField.classList.add('invalide', 'error-pattern');
                 validReturn = false;
                 break;
             case 2:
-                loginField.classList.add('invalide');
-                loginField.classList.add('error-min-login');
+                loginField.classList.add('invalide', 'error-min-login');
                 validReturn = false;
                 break;
             case 3:
-                loginField.classList.add('invalide');
-                loginField.classList.add('error-min-login');
-                loginField.classList.add('error-pattern');
+                loginField.classList.add('invalide', 'error-min-login', 'error-pattern');
                 validReturn = false;
                 break;
             case 4:
-                loginField.classList.add('invalide');
-                loginField.classList.add('error-max-login');
+                loginField.classList.add('invalide', 'error-max-login');
                 validReturn = false;
                 break;
             case 5:
-                loginField.classList.add('invalide');
-                loginField.classList.add('error-max-login');
-                loginField.classList.add('error-pattern');
+                loginField.classList.add('invalide', 'error-max-login', 'error-pattern');
                 validReturn = false;
                 break;
         }
@@ -236,16 +217,10 @@ function validateFieldLogin(loginField) {
 function validateFieldPassword(pwdField) {
     const pwdInputField = pwdField.querySelector('input');
     const pwdValue = pwdInputField.value;
-    pwdField.classList.remove('valide');
-    pwdField.classList.remove('invalide');
-    pwdField.classList.remove('error-required');
-    pwdField.classList.remove('error-min-login');
-    pwdField.classList.remove('error-max-login');
-    pwdField.classList.remove('error-pattern');
+    removeClassLists(pwdField);
     let validReturn = true;
     if (pwdValue.trim() === '') {
-        pwdField.classList.add('invalide');
-        pwdField.classList.add('error-required');
+        pwdField.classList.add('invalide', 'error-required');
         validReturn = false;
     } else {
         const pwdStatus = isContentValid(pwdValue); 
@@ -254,30 +229,23 @@ function validateFieldPassword(pwdField) {
                 pwdField.classList.add('valide');
                 break;
             case 1:
-                pwdField.classList.add('invalide');
-                pwdField.classList.add('error-pattern');
+                pwdField.classList.add('invalide', 'error-pattern');
                 validReturn = false;
                 break;
             case 2:
-                pwdField.classList.add('invalide');
-                pwdField.classList.add('error-min-login');
+                pwdField.classList.add('invalide', 'error-min-login');
                 validReturn = false;
                 break;
             case 3:
-                pwdField.classList.add('invalide');
-                pwdField.classList.add('error-min-login');
-                pwdField.classList.add('error-pattern');
+                pwdField.classList.add('invalide', 'error-min-login', 'error-pattern');
                 validReturn = false;
                 break;
             case 4:
-                pwdField.classList.add('invalide');
-                pwdField.classList.add('error-max-login');
+                pwdField.classList.add('invalide', 'error-max-login');
                 validReturn = false;
                 break;
             case 5:
-                pwdField.classList.add('invalide');
-                pwdField.classList.add('error-max-login');
-                pwdField.classList.add('error-pattern');
+                pwdField.classList.add('invalide', 'error-max-login', 'error-pattern');
                 validReturn = false;
                 break;
         }
@@ -291,13 +259,9 @@ function validateFieldPasswordConfirmation(pwdField, pdwConfField) {
     const pwdConfInputField = pdwConfField.querySelector('input');
     const pwdConfValue = pwdConfInputField.value;
     let validReturn = true;
-    pdwConfField.classList.remove('valide');
-    pdwConfField.classList.remove('invalide');
-    pdwConfField.classList.remove('error-required');
-    pdwConfField.classList.remove('error-different-pwd');
+    removeClassLists(pdwConfField);
     if (pwdConfValue.trim() == '') {
-        pdwConfField.classList.add('invalide');
-        pdwConfField.classList.add('error-required');
+        pdwConfField.classList.add('invalide', 'error-required');
         validReturn = false;
     } else {
         const status = (pwdConfStatus(pwdValue, pwdConfValue));
@@ -310,8 +274,7 @@ function validateFieldPasswordConfirmation(pwdField, pdwConfField) {
                 validReturn = false;
                 break;
             case 2:
-                pdwConfField.classList.add('invalide');
-                pdwConfField.classList.add('error-different-pwd');
+                pdwConfField.classList.add('invalide', 'error-different-pwd');
                 validReturn = false;
                 break;
         }
@@ -350,13 +313,15 @@ function setPasswordStrength(password) {
  * @returns 0-5 cases, 0 = only valid case
  */
 function isContentValid(content) { 
-    if (content.length >= 3 && content.length <= 10) {
+    const min = 6;
+    const max = 10;
+    if (content.length >= min && content.length <= max) {
         if (!isPatternWrong(content)) {
             return 0; //Valid
         } else {
             return 1; //Right length but doesn't match pattern 
         }
-    } else if (content.length < 3) { 
+    } else if (content.length < min) { 
         if (!isPatternWrong(content)) {
             return 2;   //Too short but matches pattern 
         } else {
@@ -390,18 +355,13 @@ function isPatternWrong(string) {
 function validateFieldEmail(emailField) {
     const emailInputField = emailField.querySelector('input');
     const emailValue = emailInputField.value;
-    emailField.classList.remove('valide');
-    emailField.classList.remove('invalide');
-    emailField.classList.remove('error-required');
-    emailField.classList.remove('error-email');
+    removeClassLists(emailField);
     let validReturn = true;
     if (emailValue.trim() === '') {
-        emailField.classList.add('invalide');
-        emailField.classList.add('error-required');
+        emailField.classList.add('invalide', 'error-required');
         validReturn = false;
     } else if (!isEmailValid(emailValue)) {
-        emailField.classList.add('invalide');
-        emailField.classList.add('error-email');
+        emailField.classList.add('invalide', 'error-email');
         validReturn = false;
     } else {
         emailField.classList.add('valide');
@@ -429,24 +389,17 @@ function validateFieldName(nameField) {
     const nameInputField = nameField.querySelector('input');
     const value = nameInputField.value; 
     let validReturn = true;
-    nameField.classList.remove('valide');
-    nameField.classList.remove('invalide');
-    nameField.classList.remove('error-required');
-    nameField.classList.remove('error-min-length');
-    nameField.classList.remove('error-max-length');
+    removeClassLists(nameField);
     if (value.trim() === '') {
-        nameField.classList.add('invalide');
-        nameField.classList.add('error-required');
+        nameField.classList.add('invalide', 'error-required');
         validReturn = false;
     } else {
         const valid = isNameValid(value);
         if (valid === 1) {
-            nameField.classList.add('invalide');
-            nameField.classList.add('error-min-length');
+            nameField.classList.add('invalide', 'error-min-length');
             validReturn = false;
         } else if(valid === 2) {
-            nameField.classList.add('invalide');
-            nameField.classList.add('error-max-length');
+            nameField.classList.add('invalide', 'error-max-length');
             validReturn = false;
         } else {
             nameField.classList.add('valide');
@@ -464,21 +417,16 @@ function validateFieldFirstName(firstnameField) {
     const firstnameInputField = firstnameField.querySelector('input');
     const value = firstnameInputField.value; 
     let validReturn = true;
-    firstnameField.classList.remove('valide');
-    firstnameField.classList.remove('invalide');
-    firstnameField.classList.remove('error-min-length');
-    firstnameField.classList.remove('error-max-length');
+    removeClassLists(firstnameField);
     if (value.trim() === '') {
         firstnameField.classList.add('valide'); //car champ facultatif
     } else {
         const valid = isNameValid(value);
         if (valid === 1) {
-            firstnameField.classList.add('invalide');
-            firstnameField.classList.add('error-min-length');
+            firstnameField.classList.add('invalide', 'error-min-length');
             validReturn = false;
         } else if (valid === 2) {
-            firstnameField.classList.add('invalide');
-            firstnameField.classList.add('error-max-length');
+            firstnameField.classList.add('invalide', 'error-max-length');
             validReturn = false;
         } else {
             firstnameField.classList.add('valide');
@@ -491,9 +439,11 @@ function validateFieldFirstName(firstnameField) {
  * Fonction validant les règles du champ nom
  */
 function isNameValid(name) {
-    if (name.length >= 3 && name.length <= 50) {
+    const min = 3;
+    const max = 50;
+    if (name.length >= min && name.length <= max) {
         return 0;
-    } else if (name.length < 3) {
+    } else if (name.length < min) {
         return 1;
     } else {
         return 2;
@@ -509,26 +459,19 @@ function validateFieldDate(dateField) {
     const dateInputField = dateField.querySelector('input'); 
     const date = dateInputField.valueAsDate; console.log('Valeur champ DATE : '+date);
     let validReturn = true;
-    dateField.classList.remove('valide');
-    dateField.classList.remove('invalide');
-    dateField.classList.remove('error-dateIsFutur');
-    dateField.classList.remove('error-ageIsOut');
-    dateField.classList.remove('error-required');
+    removeClassLists(dateField);
     if (date == null) {
-        dateField.classList.add('invalide');
-        dateField.classList.add('error-required');
+        dateField.classList.add('invalide', 'error-required');
         validReturn = false;
     } else {
         valid = isDateOfBirthValid(date);
         if (valid == 0) {
             dateField.classList.add('valide');
         } else if (valid == 1) {
-            dateField.classList.add('invalide');
-            dateField.classList.add('error-dateIsFutur');
+            dateField.classList.add('invalide', 'error-dateIsFutur');
             validReturn = false;
         } else {
-            dateField.classList.add('invalide');
-            dateField.classList.add('error-ageIsOut');
+            dateField.classList.add('invalide', 'error-ageIsOut');
             validReturn = false;
         }
     } 
