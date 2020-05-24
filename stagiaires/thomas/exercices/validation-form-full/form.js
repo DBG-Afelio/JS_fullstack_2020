efb60ce181406c5a4fcca02d1c560e3e697a567c
Thomas <thomaskouadio92@gmail.com>
2020-05-24 17:56:40 +0200

    generateur login ok

--- stagiaires/thomas/exercices/validation-form-full/form.js
+++ stagiaires/thomas/exercices/validation-form-full/form.js
@@ -14,6 +14,20 @@ const radiosGender = document.querySelectorAll("input[name='gender']");
 const submit = document.querySelector(".submit");
 let containerMessage = document.createElement("div");
 
+const listOflogins = 
+[
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""
+];
+
 
 //FUNCTIONS  
 
@@ -42,11 +56,28 @@ function getUserAge(dateDeNaissance) {
     return age
 }
 
+function generateUserLogin(nom, prenom) {
+
+    nom = nom.toLowerCase().substring(0,2);
+    prenom = prenom.toLowerCase().substring(0,1);
+    
+    let numbers = [1,2,3,4,5,6,7,8,9];
+    let randomNumbers = 
+    [ numbers[Math.floor(Math.random() * Math.floor(numbers.length))],
+      numbers[Math.floor(Math.random() * Math.floor(numbers.length))],
+      numbers[Math.floor(Math.random() * Math.floor(numbers.length))]
+    ]; 
+
+    return `${prenom}-${nom}_${randomNumbers.join("")}`
+
+}
+
 
 // REGEXES
-let numbersInTextField = /\d/g;
-let emailRegex = /^[a-zA-Z.0-9-]+@[a-zA-Z.]+[a-zA-Z]{2}$/;
-let phoneRegex =  /^([0032]{4}|[0]{1}|[+32]{3})[1-9]{6,9}$/;
+const numbersInTextField = /\d/g;
+const emailRegex = /^[a-zA-Z.0-9-]+@[a-zA-Z.]+[a-zA-Z]{2}$/;
+const phoneRegex =  /^([0032]{4}|[0]{1}|[+32]{3})[1-9]{6,9}$/;
+const loginRegex = /^[a-zA-Z!$0-9-_]{6,10}$/;
 
 //NOM
 
@@ -101,6 +132,7 @@ telephone.addEventListener("input", function() {
     }
 })
 
+// DATE DE NAISSANCE 
 
 dateDeNaissance.addEventListener("change", function() {
 
@@ -111,9 +143,19 @@ dateDeNaissance.addEventListener("change", function() {
     } else {
         dateDeNaissance.style.color = "#e74c3c";
     }
+})
 
-// DATE FORMATEE -> let dateValue = dateDeNaissance.value.split("-").reverse().join("-");   
+// LOGIN
 
+login.addEventListener("input", function() {
+
+
+
+})
+
+suggestLogin.addEventListener("click", function(e) {
+    login.value = generateUserLogin(nom.value, prenom.value);
+    e.preventDefault();
 })
 
 
