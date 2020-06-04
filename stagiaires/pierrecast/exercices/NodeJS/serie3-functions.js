b03d36d80652722d3351f12163e83bdbe1348b19
pierre-cast <p_castronovo@hotmail.com>
2020-06-04 11:26:52 +0200

    correctifs

--- stagiaires/pierrecast/exercices/NodeJS/serie3-functions.js
+++ stagiaires/pierrecast/exercices/NodeJS/serie3-functions.js
@@ -6,10 +6,13 @@
 function copyVector(tab){
     return [...tab];
 }
+
 let tab = [1,2,3];
 let tabOut = copyVector(tab); 
+console.log('Exercice 1');
+console.log('1-Avant Modif : tab' , tab, ' - tabOut', tabOut);
 tab[1] = 7;
-console.log('1- ' , tab, tabOut);
+console.log('1-Après Modif : tab' , tab,' - tabOut',  tabOut);
 
 /** 
  * fonction qui renvoit une copie d'un tableau de nombre à 2 dimension
@@ -18,14 +21,16 @@ console.log('1- ' , tab, tabOut);
 function copyMatrice(tabIn){
     let tabOut = [];
     tabIn.forEach(array => {
-        tabOut.push(array);
+        tabOut.push(copyVector(array));
     });
     return tabOut;
 }
 tab = [[3,2], [2,3], [1,8]];
 tabOut = copyMatrice(tab); 
+console.log('\nExercice 2');
+console.log('2-Avant  Modif : tab' , tab, ' - tabOut', tabOut);
 tab[1] = [7,2];
-console.log('2- ' , tab, tabOut);
+console.log('2-Après  Modif : tab' , tab, ' - tabOut', tabOut);
 
 /** 
  * Fonction qui renvoit une copie d'un tableau de nombre a 2 dimension trié sur la valeur de la première case du sous-tableau
@@ -40,4 +45,5 @@ function orderMatriceOnFirstPlace(tabIn) {
 tab = [[3,2], [2,3], [1,8]];
 tabOut = orderMatriceOnFirstPlace(tab); 
 
-console.log('2- ' , tab, tabOut);
+console.log('\nExercice 3');
+console.log('3- tab' ,  tab,' - tabOut', tabOut);
