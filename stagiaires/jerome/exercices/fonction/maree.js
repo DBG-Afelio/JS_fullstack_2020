5b6bdf31238d2eb18162a5813749455389c2fea2
Jer1990 <jerome.grand@hotmail.be>
2020-06-05 11:29:28 +0200

    maree jerome

---
+++ stagiaires/jerome/exercices/fonction/maree.js
@@ -0,0 +1,178 @@
+/*
+    - Soit une grille de x ligne y colonne composée de carré de couleur aléatoire
+    - et un bouton par couleur possible
+    - quand on clique sur un bouton on transmet la couleur du bouton au carré qui est en haut à gauche
+    - les carrés qui se trouvent à gauche, en haaut, à droite, en bas s'il sont de la couleur d'origine du carré de référence change aussi de couleur
+    - on propage l'opération
+    - Si tous les carrés sont de la même couleur c'est gagné
+*/
+const maree = document.querySelector('.maree');
+function randomNumber(min, max) { return Math.floor(Math.random() * (max - min) + min); }
+const colors = [
+    'rouge', 
+    'vert',
+    'bleu',
+    'violet'
+];
+
+
+/**
+ * Fonctions appelée au click sur un bouton qui lance la marée, teste la victoire et compte les coups
+ * fonction impure
+ * @param {string} couleur 
+ */
+function play(couleur) {
+    changeColor(getDiv(1,1).getAttribute('data-color'),couleur,getDiv(1,1));
+    if(isWin(maree.children,couleur)){
+        alert('Gagné');
+    };
+}
+
+
+/** 
+* fonctions générant les divs dans l'HTML sur base du nombre de lignes et de colonnes passées en paramètres
+* ces div aurant une couleur aléatoire tirées dans le tableaux des couleurs possibles
+* @param {string[]} tabcolors le tableau des couleurs acceptables
+* @param {number} lines le nombre de ligne
+* @param {number} column le nombre de colonnes
+* @param {HTMLDivElement} divParent le div où on l'on place les divs générés
+* @returns {DivHTMLElement[][]} un tableau à deux dimensions contenant les références des divs générés
+*           <div class="carre" data-ligne="2" data-colonne="4" data-color='rouge'></div>
+*/
+
+function generateMaree (tabColors, lines, column, divParent){
+    for (let i = 1; i < lines+1; i++) {
+        for (let j = 1; j < column+1; j++) {
+            const carre = document.createElement('div');
+            divParent.appendChild(carre);
+            carre.classList.add("carre");
+            const carreLine=i.toString();
+            const carreColumn=j.toString();
+            carre.setAttribute('data-ligne',carreLine);
+            carre.setAttribute('data-colonne',carreColumn);
+            carre.setAttribute('data-color',tabColors[randomNumber(0,tabColors.length)]);
+            carre.style.gridColumn=carreColumn;
+            carre.style.gridRow=carreLine;
+            
+        }
+    }
+}
+/**
+ * Fonction générant les boutons de choix de couleurs sur base d'un tableau des couleurs possibles
+ * Les boutons générés écoute l'évènement click
+ * @param {string[]} tabColors 
+ */
+const buttons = document.querySelector('.buttons');
+function generateButtons(tabColors){
+    for (let i = 0; i < tabColors.length; i++) {
+        const button=document.createElement('input');
+        buttons.appendChild(button);
+        button.setAttribute('type', 'button');
+        button.setAttribute('data-color',tabColors[i]);
+        button.addEventListener('click',(event)=>play(event.target.getAttribute('data-color')));
+    }
+    
+}
+
+/**
+ * Fonction permettant de changer la couleur d'un div passé en paramètre si celui-ci possède la couleur de oldColor 
+ * La fonction s'assurera aussi d' exécuter la même fonction sur les div haut, bas, gauche, droite s'ils existent
+ * @param {string} oldColor ancienne couleur
+ * @param {string} newColor nouvelle couleur
+ * @param {HTMLDivElement} div le div qui doit changer de couleur
+ */
+function changeColor(oldColor, newColor, div){
+    const tabDiv=[getHaut(div),getBas(div),getDroite(div),getGauche(div)];
+    if (oldColor!==newColor) {
+        setCouleur(div,newColor);
+        
+        tabDiv.forEach(element=>{
+
+            if(element!==null){
+                if(element.getAttribute('data-color')===oldColor){
+                    setCouleur(element,newColor);
+                    changeColor(oldColor, newColor, element);
+                }
+            }
+
+        })
+
+    }
+}
+
+/**
+ * fonction renvoyant le div du dessus s'il existe
+ * @param {HTMLDivElement} div 
+ * @returns {HTMLDivElement | null} le div du dessus ou null
+ */
+function getHaut(div) {
+    const ligne=parseInt(div.getAttribute('data-ligne'),10);
+    const colonne=parseInt(div.getAttribute('data-colonne'),10);
+    return getDiv(ligne-1,colonne);
+}
+
+/**
+ * fonction renvoyant le div du dessous s'il existe
+ * @param {HTMLDivElement} div 
+ * @returns {HTMLDivElement | null} le div du dessous ou null
+ */
+function getBas(div) {
+    const ligne=parseInt(div.getAttribute('data-ligne'),10);
+    const colonne=parseInt(div.getAttribute('data-colonne'),10);
+    return getDiv(ligne+1,colonne);
+}
+
+/**
+ * fonction renvoyant le div à gauche s'il existe
+ * @param {HTMLDivElement} div 
+ * @returns {HTMLDivElement | null} le div à gauche ou null
+ */
+function getGauche(div) {
+    const ligne=parseInt(div.getAttribute('data-ligne'),10);
+    const colonne=parseInt(div.getAttribute('data-colonne'),10);
+    return getDiv(ligne,colonne-1);
+}
+
+/**
+ * fonction renvoyant le div à droite s'il existe
+ * @param {HTMLDivElement} div 
+ * @returns {HTMLDivElement | null} le div à droite ou null
+ */
+function getDroite(div) {
+    const ligne=parseInt(div.getAttribute('data-ligne'),10);
+    const colonne=parseInt(div.getAttribute('data-colonne'),10);
+    return getDiv(ligne,colonne+1);
+}
+
+/**
+ * fonction renvoyant le div sur base de son position ligne/colonne s'il existe
+ * @param {number} ligne le numéro de ligne
+ * @param {number} colonne le numéro de colonne
+ * @returns {HTMLDivElement | null} le div ou null
+ */
+function getDiv(ligne, colonne) {
+    return document.querySelector(`.carre[data-colonne="${colonne.toString()}"][data-ligne="${ligne.toString()}"]`);
+    
+}
+
+/**
+ * Fonction modifiant la couleur d'un div passé en paramètre
+ * @param {DivHTMLElement} div 
+ * @param {string} couleur 
+ */
+function setCouleur(div, couleur) {
+    div.setAttribute('data-color',couleur);
+}
+
+/**
+ * Fonction testant si tous les divs sont de la couleur passée en paramètre 
+ * @param {DivHTMLElement[]} divs le tableau de tous les divs 
+ * @param {string} couleur 
+ * @returns {boolean}
+ */
+function isWin(divs, couleur){
+    return [...divs].every(element=>element.getAttribute('data-color')===couleur);  
+}
+
+generateMaree(colors,5,5,maree);
+generateButtons(colors);
