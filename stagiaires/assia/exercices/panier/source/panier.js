dee7f5cf37cb68cd1dd6af8af7ca4d00827ef77d
AssiaRachdi <arbox00@outlook.com>
2020-06-09 15:09:09 +0200

    -

---
+++ stagiaires/assia/exercices/panier/source/panier.js
@@ -0,0 +1,11 @@
+let list_el = document.querySelector('.photo-list');
+let quantiteInput_el = document.querySelector('.input-photo-quantite');
+let commanderBtn_el = document.querySelector('.btn-commander');
+
+
+function generateAllArticlesElements(ParentEl) {
+}
+
+function createOneArticleElement(PArentEl) {
+    divParent.
+}
