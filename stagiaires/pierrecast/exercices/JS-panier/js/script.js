87353bc3fb1503b01bfb2edaf206d3ecdced9145
pierre-cast <p_castronovo@hotmail.com>
2020-06-10 16:55:03 +0200

    in progress

--- stagiaires/pierrecast/exercices/JS-panier/js/script.js
+++ stagiaires/pierrecast/exercices/JS-panier/js/script.js
@@ -1,9 +1,12 @@
 const shop = tab_img;
 const oList = document.querySelector('.block-liste');
 const oDetail = document.querySelector('.block-detail');
+const oRecap = document.querySelector('.block-recap');
 const generateDetail = () => {
-    zoom(event.target.closest('.item'));
+    zoom(event.currentTarget.closest('.item'));
 }
+
+const panier = [];
 
 showList(shop);
 
@@ -55,8 +58,6 @@ function getArticleFromId(id) {
     shop.forEach(article => {
         if (article.id === Number(id)) {
             seletedArticle = article;
-            console.log(seletedArticle);
-            
         }
     });
 
@@ -69,11 +70,10 @@ function zoom(oArticle) {
     let id = Number(oArticle.getAttribute('data-id')); 
     console.log('id' ,id)
     let seletedArticle = getArticleFromId(id);
-    /*console.log("id", seletedArticle.id);*/
-    console.log( seletedArticle);
+
     oDetail.innerHTML = '';
     if (seletedArticle !== null) {
-        alert(seletedArticle.titre);
+        generateZoom(seletedArticle);
     }
 }
 
@@ -85,3 +85,102 @@ function activeArticle(oArticle) {
     oArticle.classList.add('active');
 }
 
+function generateZoom(article) {
+    let oTitle = document.createElement('h2');
+    oTitle.appendChild(document.createTextNode(article.titre));
+    oDetail.appendChild(oTitle);
+    
+    let oImg = document.createElement('img');
+    oImg.classList.add('img-zoom');
+    oImg.setAttribute('src', 'img/'+article.image.grande);
+    oImg.setAttribute('alt', article.titre)
+    oDetail.appendChild(oImg);
+
+    let oOrigin = document.createElement('p');
+    oOrigin.appendChild(document.createTextNode(`De ${article.auteur}, ${article.Pays}`));
+    oDetail.appendChild(oOrigin);
+
+    let oComment = document.createElement('p');
+    oComment.appendChild(document.createTextNode(article.commentaire))
+    oDetail.appendChild(oComment);
+
+    generatePrice(article, oDetail);
+    generateInput(article, oDetail);
+    generateButton(article, oDetail);
+}
+
+function generateInput(article) {
+    let oInput = document.createElement('input');
+    oInput.setAttribute('type', 'number');
+    oInput.setAttribute('data-id', article.id);
+    oDetail.appendChild(oInput);
+}
+
+function generateButton() {
+    let oButton = document.createElement('button');
+    oButton.appendChild(document.createTextNode('commander'));
+    oDetail.appendChild(oButton);
+    oButton.addEventListener('click', sendQuantity);   
+}
+
+function sendQuantity() {
+    let oInput = document.querySelector('.block-detail input');
+    let qte = Number(oInput.value);
+    let id = oInput.dataset.id;
+    if (qte > 0) {
+        commandArticle(id, qte);    
+    } else if (qte === 0) {
+        removeArticle(id);
+    }
+}
+
+function commandArticle(id, qte) {
+    console.log('commande id', id);
+    let article = getArticleFromId(id);
+    let present = false;
+    panier.forEach(command => {
+        if (command.article.id === article.id) {
+            present = true;
+            command.qte = qte;
+            alert('modify');
+        }
+    });
+
+    if (!present) {
+        addArticle(article, qte);   
+    }
+
+    console.log(panier);
+}
+
+function addArticle(article, qte) {
+    alert('add');
+    let command = {
+        article: article,
+        qte, qte
+    };
+
+    // ajouter dans Objet 
+    panier.push(command);
+
+    // affiche dans block-panier
+    oRecap.append(createItem(command));
+}
+
+function removeArticle(article) {
+    alert('remove')
+    panier.filter(command => command.article.id !== article.id );
+    console.log(panier);
+}
+
+function createItem(command) {
+    let oItem = document.createElement('div');
+    oItem.classList.add('item');
+
+    let oImg = document.createElement('img');
+    oImg.setAttribute('src', 'img/'+command.article.image.petite);
+    oImg.setAttribute('alt', command.article.title);
+    oItem.append(oImg) ;
+
+    return oItem;
+}
