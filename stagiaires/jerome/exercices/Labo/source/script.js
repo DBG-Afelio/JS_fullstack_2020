7d87f5755397471d7eb13ab0e9dabe44b894914c
jer1990 <jerome.grand@hotmail.be>
2020-06-11 16:26:30 +0200

    milieu labo

--- stagiaires/jerome/exercices/Labo/source/script.js
+++ stagiaires/jerome/exercices/Labo/source/script.js
@@ -1,7 +1,16 @@
-//const article=document.querySelector('.article');
-//fonction d'ajout d'article...........................................................
+
 let articles=document.querySelector('.articles');
 let detail=document.querySelector('.detail');
+let main=document.querySelector('main');
+let quantArt=document.querySelector('.quantArt');
+let commander=document.querySelector('.commander');
+let somme=document.querySelector('.somme');
+let nbrArticles=document.querySelector('.quantite')
+let divArticle;
+let sum=0;
+let tabPanier=[];
+//fonction d'ajout d'article...........................................................
+
 
 function articleCreation(tab){
     tab.forEach(element => {
@@ -21,12 +30,22 @@ function articleCreation(tab){
 
     });
 }
-//fonction creation panier...........................................................
+
 //fonction creation detail...........................................................
+
 function detailArticle(object){
-   
+    document.querySelector('main').innerHTML="";
+    let detail=document.createElement('div');
+    main.appendChild(detail);
+    detail.classList.add('detail');
     let div=document.createElement('div');
     detail.appendChild(div);
+    div.classList.add('divArticle');
+    div.setAttribute('data-id',object.id);
+    divArticle=document.querySelector('.divArticle');
+
+    
+
     
     const titre=document.createElement('span');
 
@@ -39,12 +58,15 @@ function detailArticle(object){
     const pays=document.createElement('span');
 
     const imgArticle=document.createElement('img');
-
-    div.appendChild(imgArticle);
-    imgArticle.setAttribute('src',`img/${object.image.grande}`);
+    
 
     div.appendChild(titre);
     titre.innerHTML=object.titre;
+
+
+    div.appendChild(imgArticle);
+    imgArticle.setAttribute('src',`img/${object.image.moyenne}`);
+
 
     div.appendChild(prix);
     prix.innerHTML=object.Prix;
@@ -61,17 +83,58 @@ function detailArticle(object){
 }
 
 
+//fonction creation panier...........................................................
+function ajoutArticle(object){
+    let quantite=parseInt(quantArt.value);
+    let articleFind=tabPanier.find(article=>object.id==article.id);
+    if (articleFind) {
+        articleFind.quantite=quantite;
+    } else {
+        tabPanier.push({
+
+            id:object.id,
+            titre:object.titre,
+            prix:parseFloat(object.Prix),
+            quantite,
+
+            })
+    }
+    console.log(tabPanier);
+}
+function prixTotalPanier(){
+
+   return tabPanier.reduce((sum,article)=>sum+article.prix*article.quantite,0); 
+
+}
+
+function quantiteArticlePanier(){
+    return tabPanier.reduce((sum,article)=>sum+article.quantite,0); 
+}
+
+function affichagePanier(){
+    somme.innerHTML=prixTotalPanier();
+    nbrArticles.innerHTML=quantiteArticlePanier();
+}
 
 //Appel de fonctions...........................................................
 
 articleCreation(tab_img);
 let articleSelect=document.querySelectorAll('.article');
-for (const art of articleSelect) {
-    art.addEventListener('click',eventClick)
-}
-function eventClick(event){
-    let imageObject=tab_img.find(object=> event.currentTarget.getAttribute('data-id')==object.id );
 
-    detailArticle(imageObject);
-    console.log(event.currentTarget.getAttribute('data-id'));
+for (const art of articleSelect) {
+    art.addEventListener('click',(event)=>detailArticle(getObject(event.currentTarget.dataset.id)));
+    
 }
+
+function getObject(id){
+    return tab_img.find(object=> parseInt(id)==object.id );  
+}
+
+detailArticle(getObject(1));
+
+commander.addEventListener('click',()=>{
+
+    ajoutArticle(getObject(divArticle.dataset.id));
+    affichagePanier();
+    
+});
