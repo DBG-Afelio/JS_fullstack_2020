8e36639f962fcb55f55f8a02ac9f6c353c734e60
AssiaRachdi <arbox00@outlook.com>
2020-06-15 17:15:19 +0200

    recup from first draft

---
+++ stagiaires/assia/exercices/panier-reworked-after-demo/source/sourceJSON.js
@@ -0,0 +1,142 @@
+tab_img=
+  [{
+      titre         : "Bulles",
+      auteur        : "E. Lassaux",
+      commentaire   : "Spécialisé dans les photos d'objet, \"Neness\" aime aussi photographier les animaux",
+      Pays          : "Suède",
+      Prix          : "208 euros",
+      image         :   {
+                        grande          : "bulle_gd.jpg",
+                        moyenne         : "bulle_pt.jpg",
+                        petite          : "bulle_tpt.jpg",
+                        toute_petite    : "bulle_ttpt.jpg"
+                          },
+      id :1
+    },
+    {
+      titre : "L'Oeil du tig",
+      auteur : "J. Herman",
+      commentaire : "Tout droit sorti de la nouvelle école Portugaise, il nous donne là une de ses plus belle oeuvres",
+      Pays : "Brasil",
+      Prix : "228 euros",
+      image : {
+        grande : "escalier_gd.jpg",
+        moyenne : "escalier_pt.jpg",
+        petite : "escalier_tpt.jpg",
+        toute_petite : "escalier_ttpt.jpg"
+                  },
+      id :2
+    },
+    {
+      titre : "Escape",
+      auteur : "JTjiel",
+      commentaire : "Ce Finlandais un peu déjanté, ouvre son coeur et son objectif",
+      Pays : "Helsinski",
+      Prix : "28 euros",
+      image : {
+        grande : "fenetre_gd.jpg",
+        moyenne : "fenetre_pt.jpg",
+        petite : "fenetre_tpt.jpg",
+        toute_petite : "fenetre_ttpt.jpg"
+                  },
+      id :3
+    },
+    {
+      titre : "Intimité",
+      auteur : "DBoo",
+      commentaire : "Ce sympatique gailliard, nous invite chez lui en toute simplicité",
+      Pays : "Helsinski",
+      Prix : "28 euros",
+      image : {
+        grande : "interieur_gd.jpg",
+        moyenne : "interieur_pt.jpg",
+        petite : "interieur_tpt.jpg",
+        toute_petite : "interieur_ttpt.jpg"
+                  },
+      id :4
+    },
+    {
+      titre : "Palettes",
+      auteur : "Sabina",
+      commentaire : "Oeuvre originale issue d'une artiste du sac",
+      Pays : "Casablanka",
+      Prix : "245 euros",
+      image : {
+        grande : "pantone_gd.jpg",
+        moyenne : "pantone_pt.jpg",
+        petite : "pantone_tpt.jpg",
+        toute_petite : "pantone_ttpt.jpg"
+                  },
+      id :5
+    },
+    {
+      titre : "Untitled",
+      auteur : "Anonyme",
+      commentaire : "Oeuvre anonyme...",
+      Pays : "Bruxelles",
+      Prix : "8 euros",
+      image : {
+        grande : "roul_gd.jpg",
+        moyenne : "roul_pt.jpg",
+        petite : "roul_tpt.jpg",
+        toute_petite : "roul_ttpt.jpg"
+                  },
+      id :6
+    },
+    
+    {
+      titre : "Tourne tourne petit escalier",
+      auteur : "Marie N.",
+      commentaire : "Première photo d'une série d'une artiste trop vite disparue",
+      Pays : "Huy",
+      Prix : "43 euros",
+      image : {
+        grande : "tourne_gd.jpg",
+        moyenne : "tourne_pt.jpg",
+        petite : "tourne_tpt.jpg",
+        toute_petite : "tourne_ttpt.jpg"
+                  },
+      id :7
+    },
+    {
+      titre : "Reflets",
+      auteur : "Coppe",
+      commentaire : "Cette photo un peu mégalo nous illustre la démesure de ce brave roumain",
+      Pays : "Belgrade",
+      Prix : "2.8 euros",
+      image : {
+        grande : "vitres_gd.jpg",
+        moyenne : "vitres_pt.jpg",
+        petite : "vitres_tpt.jpg",
+        toute_petite : "vitres_ttpt.jpg"
+                  },
+      id :8
+    },
+    {
+      titre : "Le lait tue",
+      auteur : "S. alade",
+      commentaire : "Du rêve dans un légume...",
+      Pays : "Bressoux",
+      Prix : "13 euros",
+      image : {
+        grande : "laitue_gd.jpg",
+        moyenne : "laitue_pt.jpg",
+        petite : "laitue_tpt.jpg",
+        toute_petite : "laitue_ttpt.jpg"
+                  },
+      id :9
+    },
+    {
+      titre : "Buildings",
+      auteur : "Toto",
+      commentaire : "Du grand, du lourd",
+      Pays : "Varsovie",
+      Prix : "12 euros",
+      image : {
+        grande : "immeuble_gd.jpg",
+        moyenne : "immeuble_pt.jpg",
+        petite : "immeuble_tpt.jpg",
+        toute_petite : "immeuble_ttpt.jpg"
+                  },
+      id :10
+    }]
