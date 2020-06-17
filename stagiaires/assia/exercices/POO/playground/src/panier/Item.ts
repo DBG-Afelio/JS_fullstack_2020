b13e33cb985ff4c9cc9a0f21189edb540c5e0e34
AssiaRachdi <arbox00@outlook.com>
2020-06-17 16:21:13 +0200

    -

---
+++ stagiaires/assia/exercices/POO/playground/src/panier/Item.ts
@@ -0,0 +1,30 @@
+import { ItemStock } from "./ItemStock";
+
+export class Item {
+    private item: object;
+
+    constructor(item:object) {
+        this.item = 
+    }
+}
+
+
+// class item
+    //set valeurs sur base de tab_img
+
+    //methodes : 
+//createItem
+//createList
+//displayItemList
+//displayDetailView
+
+// class basket : itemOrdered, totalPrice, totalQtty
+    //get value en public
+    //set value en fonction de itemOrdered
+    //methodes :
+//addItemIntoBasket
+//updateQttyItem
+//removeItemFromBasket
+//emptyBasket
+
+ 
---
+++ stagiaires/assia/exercices/POO/playground/src/panier/ShoppingCart.ts
@@ -0,0 +1,47 @@
+export class ShoppingCart {
+
+    constructor() {
+        
+    }
+
+    //methodes:
+
+    // ajouter un article avec 
+    addItem() {
+        
+    }
+    //retirer un article, devra renvoyer false si l'item que l'on essaye de supprimer n'existe pas;
+    removeItem() {
+        
+    }
+
+    //supprimer tout l'article du panier
+    deleteItem() {
+        
+    }
+
+    //vider le panier total
+    emptyCart() {
+        
+    }
+
+    //calculer le nombre d'articles dns le panier
+    setCartCount() {
+        
+    }
+
+    //connaître le nombre d'articles avec 
+    getCartCount() {
+        
+    }
+
+    //calculer le prix total du panier avec 
+    setTotalPrice() {
+
+    }
+
+    //connaitre le prix total du panier avec 
+    getTotalPrice() {
+
+    }
+}
