c65ef8818e8b55693ef5e830052d19bc45672418
stagiaire <acoma1@protonmail.com>
2020-06-24 00:06:40 +0200

    angular

---
+++ stagiaires/pierrep/exercices/angular/tour-of-hero/src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+
+
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+}
---
+++ stagiaires/pierrep/exercices/angular/tour-of-hero/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', class: 'Mage' },
+  { id: 12, name: 'Narco', class: 'Mage' },
+  { id: 13, name: 'Bombasto', class: 'Mage' },
+  { id: 14, name: 'Celeritas', class: 'Mage' },
+  { id: 15, name: 'Magneta', class: 'Mage' },
+  { id: 16, name: 'RubberMan', class: 'Mage' },
+  { id: 17, name: 'Dynama', class: 'Mage' },
+  { id: 18, name: 'Dr IQ', class: 'Mage' },
+  { id: 19, name: 'Magma', class: 'Mage' },
+  { id: 20, name: 'Tornado', class: 'Mage' }
+];
