05285550db6dcf0caae9e567ded111e001e7e4be
stagiaire <acoma1@protonmail.com>
2020-06-25 15:22:26 +0200

    blog

---
+++ stagiaires/pierrep/exercices/angular/OC-init/OC-blog/OC-init-blog/src/app/post-list-component/post-list-component.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-component',
+  templateUrl: './post-list-component.component.html',
+  styleUrls: ['./post-list-component.component.css']
+})
+export class PostListComponentComponent implements OnInit {
+
+  constructor() { 
+    console.log("ss"); 
+  }
+
+
+
+  ngOnInit(): void {
+  }
+
+}
