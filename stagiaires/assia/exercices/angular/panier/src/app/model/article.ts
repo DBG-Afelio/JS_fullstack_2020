0ff2754e06b531a01fb790713b02bfdeaaaa07a1
AssiaRachdi <arbox00@outlook.com>
2020-07-04 14:58:19 +0200

    -

--- stagiaires/assia/exercices/angular/panier/src/app/model/article.ts
+++ stagiaires/assia/exercices/angular/panier/src/app/model/article.ts
@@ -1,11 +1,11 @@
 export class Article {
-    private titre: string;
-    private auteur: string;
-    private commentaire: string;
-    private pays: string;
-    private prix: number;
-    private image: any;
-    private id: number;
+    public titre: string;
+    public auteur: string;
+    public commentaire: string;
+    public pays: string;
+    public prix: number;
+    public image: any;
+    public id: number;
     constructor(titre: string, auteur: string, commentaire: string, pays: string, prix: number, image: any, id: number) {
         this.titre = titre;
         this.auteur = auteur;
@@ -16,27 +16,27 @@ export class Article {
         this.id = id;
     }
 
-    getTitre(): string {
-        return this.titre;
-    }
-    getAuteur(): string {
-        return this.auteur;
-    }
-    getComments(): string {
-        return this.commentaire;
-    }
-    getPays(): string {
-        return this.pays;
-    }
-    getPrix(): number {
-        return this.prix;
-    }
-    getImage(): any {
-        return this.image;
-    }
-    // type Image = { grande: string, moyenne: string, petite: string, toute_petite: string };
+    // getTitre(): string {
+    //     return this.titre;
+    // }
+    // getAuteur(): string {
+    //     return this.auteur;
+    // }
+    // getComments(): string {
+    //     return this.commentaire;
+    // }
+    // getPays(): string {
+    //     return this.pays;
+    // }
+    // getPrix(): number {
+    //     return this.prix;
+    // }
+    // getImage(): any {
+    //     return this.image;
+    // }
+    // // type Image = { grande: string, moyenne: string, petite: string, toute_petite: string };
 
-    getId(): number {
-        return this.id;
-    }
+    // getId(): number {
+    //     return this.id;
+   // }
 }
--- stagiaires/assia/exercices/angular/panier/src/app/pages/e-shop-page/e-shop-page.component.ts
+++ stagiaires/assia/exercices/angular/panier/src/app/pages/e-shop-page/e-shop-page.component.ts
@@ -11,9 +11,9 @@ import { View } from 'src/app/model/view.enum';
   styleUrls: ['./e-shop-page.component.css']
 })
 export class EShopPageComponent implements OnInit {
-  public articlesStock: Article[];
-  public articlesCom: ArticleCommande[];
-  public viewEshop = View.ESHOP ;
+  public articlesStock: Article[]=[];
+  public articlesCom: ArticleCommande[]=[];
+  public viewEshop = View.ESHOP;
   public articleSelected: Article;
 
   constructor(
@@ -28,14 +28,20 @@ export class EShopPageComponent implements OnInit {
     // })
     this.stockService.getArticlesStock().subscribe((stockRecue) => {
       this.articlesStock = stockRecue;
-      console.log('stock recu : ', this.articlesStock);
     })
     this.articleSelected = this.articlesStock[0];
-    console.log('selection : ', this.articleSelected);
   }
   updateSelection(art: Article): void {
     this.articleSelected = art;
   }
+
+  findArticleDansPanier(): ArticleCommande | undefined {
+    return this.articlesCom.find(artCom => artCom?.getArticle() === this.articleSelected);
+  }
+
+  updatePanier(qt: number): void{
+    console.log('qtty : ', qt, ' to save in Panier for article : ', this.articleSelected);
+}
   updateArticle(articleCom: ArticleCommande): void {
 
   }
