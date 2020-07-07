5d485b049bc563cffed0fc729e35808043ee3820
Haroon <32717338+har1991@users.noreply.github.com>
2020-07-07 10:59:55 +0200

    haroon dto

--- groupes/delta/ng-proforma-app/src/model/fourn.ts
+++ groupes/delta/ng-proforma-app/src/model/fourn.ts
@@ -1,3 +1,6 @@
+import { FournDto } from './fournDto';
+
+
 export class Fourn {
 
 
@@ -9,7 +12,7 @@ export class Fourn {
         public archive : boolean ,
         public horaire :boolean[] ,
         public tel : string ){
-            
+
             this.id = id ;
             this.nom = nom ;
             this.description = description ;
@@ -18,6 +21,29 @@ export class Fourn {
             this.horaire = horaire ;
             this.tel = tel ; 
             
+    }
+    static fromDto(fournDto:FournDto):Fourn{
+        return new Fourn(
+            fournDto.id ,
+            fournDto.nom,
+            fournDto.description,
+            fournDto.ferme,
+            fournDto.archive,
+            fournDto.horaire,
+            fournDto.tel
+        )
+    }
+    toDto():FournDto {
+        return { 
+            id : this.id,
+            nom : this.nom , 
+            description : this.description,
+            ferme : this.ferme ,
+            archive:this.archive,
+            horaire : this.horaire,
+            tel : this.tel
+        }
 
     }
+
 }
