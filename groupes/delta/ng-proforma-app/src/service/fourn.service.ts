68068650b51fc1f0d2d67fd143b2d12dd0d569d9
Haroon <32717338+har1991@users.noreply.github.com>
2020-07-07 11:23:26 +0200

    HatoonGetId

--- groupes/delta/ng-proforma-app/src/service/fourn.service.ts
+++ groupes/delta/ng-proforma-app/src/service/fourn.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { FournDto } from 'src/model/fournDto';
+import { map } from 'rxjs/operators';
+import { Fourn } from 'src/model/fourn';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FournService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
 
 
   getFournList():Observable<Fourn[]> {
-
+    return this.http.get<FournDto[]>('http://localhost:3000/fournisseurs').pipe(
+      map((fourns:FournDto[]) => {
+        return fourns.map((fourn : FournDto)=>Fourn.fromDto(fourn));
+      })
+    )
   }
-  getFournById(){
-
+  getFournById(id : number):Observable <Fourn>{
+    return this.http.get<FournDto>(`http://localhost:3000/fournisseurs/${id}`).pipe(
+      map((fourn : FournDto)=>{
+        return Fourn.fromDto(fourn);
+      })
+    )
   }
 
 
