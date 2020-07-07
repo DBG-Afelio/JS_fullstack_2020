af3e19214d0b50f031f59ac0afaaf32ce71c2507
pierre-cast <p_castronovo@hotmail.com>
2020-07-07 14:05:22 +0200

    paths

--- exercice/mvp/alpha/application/commandeSandwitch/src/app/app-routing.module.ts
+++ exercice/mvp/alpha/application/commandeSandwitch/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : "homepage", component: HomePageComponent },
+  { path : "", redirectTo: "/homePage" , pathMatch: "full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
