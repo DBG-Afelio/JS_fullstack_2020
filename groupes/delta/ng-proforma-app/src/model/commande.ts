8c3c00f89c29063c04ad336a1744db24a4d234b1
BlackScaryZ <blackscaryz@gmail.com>
2020-07-08 12:02:25 +0200

    Init commande service

--- groupes/delta/ng-proforma-app/src/model/commande.ts
+++ groupes/delta/ng-proforma-app/src/model/commande.ts
@@ -1,10 +1,53 @@
 import { User } from './user';
 import { Option } from './option';
 import { Product } from './product';
+import { CommandeDto } from './commandeDto';
 
 export class Commande {
     date: Date;
     user: User;
     product: Product;
-    options: Option;
+    options: Option[];
+
+    constructor(public paye:boolean,
+        public id:number,
+        public option_ids: number[]
+    ) {
+        this.paye = paye;
+        this.id = id;
+        this.option_ids = option_ids;
+    }
+
+    static fromDto(commandeDto: CommandeDto) {
+        return new Commande(commandeDto.paye,commandeDto.id,commandeDto.option_ids);
+    }
+
+    toDto(): CommandeDto {
+        return {
+            user_id: this.user.id,
+            product_id: this.product.id,
+            option_ids: this.option_ids,
+            paye: this.paye,
+            id: this.id,
+            date: this.date.toJSON()
+        }
+    }
+
+    setAll(date:Date,user:User,product:Product,options:Option[]):Commande {
+        this.date = date;
+        this.user = user;
+        this.product = product;
+        this.options = options;
+        return this;
+    }
+
+    setDate(date:Date):Commande {
+        this.date = date;
+        return this;
+    }
+
+    setUser(user:User):Commande {
+        this.user = user;
+        return this;
+    }
 }
--- groupes/delta/ng-proforma-app/src/service/commandes.service.ts
+++ groupes/delta/ng-proforma-app/src/service/commandes.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, mergeMap } from "rxjs/operators";
+import { Commande } from 'src/model/commande';
+import { HttpClient } from '@angular/common/http';
+import { CommandeDto } from 'src/model/commandeDto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommandesService {
 
-constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCommandeById(id:number): Observable<Commande> {
+    return this.http.get<CommandeDto>(`http://localhost:3000/commandes/${id}`).pipe(
+      map((commandeDto:CommandeDto) => {
+        return Commande.fromDto(commandeDto).setDate(new Date(commandeDto.date));
+      })
+    )
+  }
+
 
 }
