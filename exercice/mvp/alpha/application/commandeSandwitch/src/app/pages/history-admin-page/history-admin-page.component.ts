f7584a34bf74b11ad3335c5cfdc7b031065fb01c
AssiaRachdi <arbox00@outlook.com>
2020-07-14 19:18:22 +0200

    -

--- exercice/mvp/alpha/application/commandeSandwitch/src/app/pages/history-admin-page/history-admin-page.component.ts
+++ exercice/mvp/alpha/application/commandeSandwitch/src/app/pages/history-admin-page/history-admin-page.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FullOrder } from 'src/app/models/fullOrderModel/fullOrder';
 import { OrderService } from 'src/app/services/orderService/order.service';
-import { SupplierService } from 'src/app/services/supplierService/supplier.service';
 import { Supplier } from 'src/app/models/supplierModel/Supplier';
-import { NgModule, LOCALE_ID } from '@angular/core';
 @Component({
   selector: 'app-history-admin-page',
   templateUrl: './history-admin-page.component.html',
@@ -14,15 +12,26 @@ export class HistoryAdminPageComponent implements OnInit {
   public allOrders: FullOrder[] = [];
   public mySupplier: Supplier = null;
   
+  public page: number;
+  public pageSize: number;
+  public collectionSize: number;
+
   constructor(
     private orderService: OrderService,
-    private supplierService: SupplierService
   ) {
-    this.orderService.getAllFullOrders().subscribe((fullHistory) => this.allOrders = fullHistory);
+    this.orderService.getAllFullOrders().subscribe((fullHistory) => {
+      this.allOrders = fullHistory;
+      this.collectionSize = fullHistory.length;
+    });
+    this.page = 1;
+    this.pageSize = 10;
   }
 
   ngOnInit(): void {
   }
 
+  public sortBy(prop: string){
+    return this.allOrders.sort((a, b) => a[prop] > b[prop] ? -1 : a[prop] === b[prop] ? 0 : 1);
+  }
   
 }
