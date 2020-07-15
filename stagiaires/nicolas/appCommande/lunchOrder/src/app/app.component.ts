f79e720f21e0cb5d912263d3417311dd53610835
Ngrdj <ngrdjhotmail.fr>
2020-07-15 12:26:49 +0200

    _

--- stagiaires/nicolas/appCommande/lunchOrder/src/app/app.component.ts
+++ stagiaires/nicolas/appCommande/lunchOrder/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UsersListService } from './services/users-list.service';
 import { User } from './models/user';
 import { OrdersListService } from './services/orders-list.service';
+import { Order } from './models/order';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,19 @@ export class AppComponent {
   title = 'lunchOrder';
   showFiller:boolean = false;
   currentUser:User;
+  currentUserOrder:Order;
 
   numberOfDailyOrder:number;
 
   constructor(private usersListService:UsersListService,
               private ordersListService:OrdersListService,){
 
-    usersListService.getCurrentUser().subscribe( userFound => this.currentUser = userFound)
+    usersListService.getCurrentUser().subscribe( userFound => {
+
+      this.currentUser = userFound
+      
+
+    })
     ordersListService.getOrdersList().subscribe( ordersListFound => {
 
       this.numberOfDailyOrder = ordersListFound.filter(order => order.date.getDate() === new Date().getDate()).length
