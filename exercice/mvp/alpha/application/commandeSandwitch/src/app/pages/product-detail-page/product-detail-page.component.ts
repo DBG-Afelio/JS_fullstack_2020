ed63208ea5126422cc884d48d6d28991534c5009
AssiaRachdi <arbox00@outlook.com>
2020-07-15 12:05:29 +0200

    fix pre-select options + updated total

--- exercice/mvp/alpha/application/commandeSandwitch/src/app/pages/product-detail-page/product-detail-page.component.ts
+++ exercice/mvp/alpha/application/commandeSandwitch/src/app/pages/product-detail-page/product-detail-page.component.ts
@@ -23,48 +23,82 @@ export class ProductDetailPageComponent implements OnInit {
   public product: Product;
   public currentUser: User = null;
   public updatedPrice: number = 0;
-  public optionsSelected: number[] = [];
+  // public optionsSelected: number[] = []; //a supprimer plutard
+  public selectedOptionsHere: Option[] = [];
   public fullOrder: FullOrder = null;
+  public isEqualToUserOrder: boolean = false;
+  public selected: number[] = [];
+
+
+
   constructor(
     public supplierService: SupplierService,
-    public productService: ProductService, 
+    public productService: ProductService,
     public activatedRoute: ActivatedRoute,
     private userService: UserService,
     private orderService: OrderService,
-    private router:Router,
-  ) { 
-    this.orderService.isOnTime().subscribe((timingStatus) => this.isOnTime = timingStatus);
-    this.activatedRoute.paramMap.subscribe(params => {
-      let id = Number(params.get('id'));
-      this.productService.getProductById(id).subscribe(product => {
-        this.product = product;
-        this.updatedPrice = this.product.price;
-      })
-    });
+    private router: Router,
+  ) {
     this.loadData();
   }
 
   ngOnInit(): void {
   }
-  public loadData(): void{
-    this.supplierService.getList().subscribe((list) => this.listSuppliers = list);
-    this.userService.getCurrentUser().subscribe((user) => {
-      this.currentUser = user;
+
+  public loadData(): void {
+    this.userService.getCurrentUser().subscribe((user) => this.currentUser = user);
+    this.activatedRoute.paramMap.subscribe(params => {
+      let id = Number(params.get('id'));
+
+      this.productService.getProductById(id).subscribe(product => {
+        this.product = product;
+        // this.product ? this.updateFinalPrice() : this.product;
+        this.orderService.getFullOrder().subscribe((full) => {
+          this.fullOrder = full;
+          if (!this.fullOrder || this.fullOrder.getProduct().id !== product.id) { 
+            this.selectedOptionsHere = this.selected = [];
+            this.updatedPrice = product.price;
+          //  this.updateFinalPrice();
+          } else {
+            this.selectedOptionsHere = this.fullOrder.getSelectedOptions();
+            this.selected = this.selectedOptionsHere.map((option) => option.id);
+            this.updateFinalPrice();
+          }
+        });
+      });
     });
-    this.orderService.getFullOrder().subscribe((full) => this.fullOrder = full);
+    this.orderService.isOnTime().subscribe((timingStatus) => this.isOnTime = timingStatus);
+    this.supplierService.getList().subscribe((list) => this.listSuppliers = list);
+    
   }
-  
-  public updateEndPrice(isOptionAdded: boolean, optionPrice: number, optionId:number): number{   
-    if (isOptionAdded) {
-      this.updatedPrice += optionPrice;
-      this.optionsSelected.push(optionId);
+
+
+  public updateFinalPrice(): void {
+    this.updatedPrice = this.product.price;
+    this.selectedOptionsHere.forEach((option) => this.updatedPrice += option.surcout);
+  }
+
+  public optionChangeRequest(changedOption: Option, optionState: boolean): void {
+    if (optionState) {
+      this.selectedOptionsHere.push(changedOption);
     } else {
-      this.updatedPrice -= optionPrice;
-      const index: number = this.optionsSelected.findIndex(id => id === optionId);
-      this.optionsSelected.splice(index, 1);
+      const index: number = this.selectedOptionsHere.findIndex((option) => option.id === changedOption.id);
+      this.selectedOptionsHere.splice(index, 1);
     }
-    return this.updatedPrice;
+    this.updateFinalPrice();
   }
+
+  public setOptionState(optionDisplayed: Option): boolean {
+    let isChecked: boolean = false;
+    if (this.fullOrder && this.fullOrder.getSelectedOptions().length > 0) {
+      this.fullOrder.getSelectedOptions().find((option) => option.id === optionDisplayed.id) ?
+        isChecked = true : isChecked = false;
+    }
+    this.optionChangeRequest(optionDisplayed, isChecked);
+    this.updateFinalPrice();
+    return isChecked;
+  }
+
 
   public saveInLocalStorage(): void {
     if (this.isOnTime) {
@@ -72,15 +106,12 @@ export class ProductDetailPageComponent implements OnInit {
         this.orderService.addInLocalStorage(this.createNewOrder());
       } else {
         if (this.fullOrder.isConfirmed()) { //une commande confirmee existe
-          const msg2 = `Vous avez deja une commande CONFIRMEE pour aujourd'hui: ${this.fullOrder.getProduct().getName()}. Veuillez la supprimer (avant 10h30) avant de renouveler une commande`;
+          const msg2 = `Vous avez deja une commande CONFIRMEE pour aujourd'hui: ${this.fullOrder.getOrderProductWithOptions()}. Veuillez la supprimer (avant 10h30) avant de renouveler une commande`;
           if (window.confirm(msg2)) {
             this.router.navigateByUrl(`/produit/${this.fullOrder.getOrder().productId}`);
           }
-        } else { //une commande en cours existe
-          const msg1 = `Vous avez deja une commande temporairement SAUVEGARDEE: ${this.fullOrder.getProduct().getName()}. Voulez-vous la remplacer ?`;
-          if (window.confirm(msg1)) {
-            this.orderService.addInLocalStorage(this.createNewOrder());
-          }
+        } else { 
+          this.orderService.addInLocalStorage(this.createNewOrder());
         }
       }
     } else {
@@ -94,13 +125,18 @@ export class ProductDetailPageComponent implements OnInit {
     return new Order(
       this.currentUser.id,
       this.product.id,
-      this.optionsSelected,
+      this.getOptionIds(this.selectedOptionsHere),
       false, //isPayed
       0,  //id = 0 pour les new
       new Date()
     );
   }
 
+  public getOptionIds(optionArr: Option[]): number[]{
+    let optionIds: number[] = [];
+    optionArr.forEach((optionObj) => optionIds.push(optionObj.getId()));
+    return optionIds;
+  }
   
   public remove(): void{
     if (this.isOnTime) {
@@ -117,7 +153,6 @@ export class ProductDetailPageComponent implements OnInit {
     } else {
       window.alert('Le temps limite est depasse. Nous sommes desoles de ne pouvoir prendre en compte votre demande.');
     }
-    
   }
 
 }
