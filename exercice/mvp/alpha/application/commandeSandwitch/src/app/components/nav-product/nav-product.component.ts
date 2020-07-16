6a9957f13cad2d0dbd279996fdf672c8b7ed28c4
pierre-cast <p_castronovo@hotmail.com>
2020-07-16 11:42:54 +0200

    nav Product OK

--- exercice/mvp/alpha/application/commandeSandwitch/src/app/components/nav-product/nav-product.component.ts
+++ exercice/mvp/alpha/application/commandeSandwitch/src/app/components/nav-product/nav-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { Supplier } from 'src/app/models/supplierModel/Supplier';
 
 @Component({
@@ -8,10 +8,60 @@ import { Supplier } from 'src/app/models/supplierModel/Supplier';
 })
 export class NavProductComponent implements OnInit {
 
+  public index: number = 0;
+  public max: number = 4;
+  public min: number = 0;
+  public left: number = 15;
+  public size: number = 3;
+
   @Input() listSuppliers: Supplier[];
-  
-  constructor() { }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.setSize(window.innerWidth);
+    console.log('size', this.size, window.innerWidth);
+  }
+
+  constructor() { 
+  }
+
+  public previous() {
+    if (this.index > this.min) {
+      this.index--;
+      this.left = 15 - (this.index * 215);
+      this.writePosition(); 
+    }
+  }
+
+  public next() {
+    this.max = (this.listSuppliers) ? this.listSuppliers.length : 4;
+
+    if (this.index + this.size < this.max) {
+      this.index++;
+      this.left =  15 - (this.index * 215);
+      this.writePosition(); 
+    } 
+      
+  }
+
+  public setSize(windowSize: number) {
+    if (windowSize >= 1550) {
+      this.size = 4;
+    } else if (windowSize >= 1250) {
+      this.size = 3;
+    } else if (windowSize >= 1000) {
+      this.size = 2;
+    } else {
+      this.size = 1;
+    }
+  }
+
+  public writePosition() {
+    console.log('indexes', this.index, this.index + this.size, this.max , '-', this.size);
+    console.log(this.left);
+  }
 
   ngOnInit(): void {
+    this.setSize(window.innerWidth);
+    this.writePosition(); 
   }
 }
