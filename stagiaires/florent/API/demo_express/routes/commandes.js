3c38d21301e6b67659cb5b7051ab51427334face
Fripon08 <62310112+Fripon08@users.noreply.github.com>
2020-08-25 10:00:20 +0200

    Commandes & produits

--- stagiaires/florent/API/demo_express/routes/commandes.js
+++ stagiaires/florent/API/demo_express/routes/commandes.js
@@ -15,4 +15,38 @@ router.get('/:id', (request, response, next) => {
     });
 });
 
+router.post('', (request, response) => {
+    console.log(request.body);
+    pool.query('INSERT INTO commandes (user_id, product_id, paye, date) VALUES ($1, $2, $3, $4)', 
+    [request.body.user_id,
+     request.body.product_id,
+     request.body.paye,
+     request.body.date
+    ], 
+    (request,response)=>{
+        response.status(200).json({message:'Requête créé!'});
+    })
+});
+
+router.put('/:id', (request,response)=>{
+    const id = parseInt(request.params.id)
+    pool.query('UPDATE commandes SET  user_id = $1, product_id = $2, paye = $3, date = $4 WHERE id = $5', 
+    [request.body.user_id,
+     request.body.product_id,
+     request.body.paye,
+     request.body.date,
+    id
+    ], 
+    (erreur,result)=>{
+        response.status(200).json({message:'Requête créé!'});
+    }) 
+});
+
+router.delete('',(request,response)=>{
+    const id = parseInt(request.params.id)
+    pool.query('DELETE FROM commandes WHERE id=$1',[id], (erreur,result) => {
+        response.json(result.rows);
+    })
+});
+
 module.exports = router;
--- stagiaires/florent/API/demo_express/routes/produits.js
+++ stagiaires/florent/API/demo_express/routes/produits.js
@@ -24,9 +24,18 @@ router.post('', (request, response) => {
      request.body.fourn_id
     ], 
     (request,response)=>{
-
+        response.status(200).json({message:'Requête créé!'});
     })
-    response.status(200).json({message:'Requête créé!'});
+    
+});
+
+router.delete('',(request,response)=>{
+    const id = parseInt(request.params.id)
+    pool.query('DELETE FROM commandes WHERE product_id=$1',[id], (erreur,result) => {
+        pool.query('DELETE FROM produits WHERE id=$1',[id],(erreur,result) =>{
+            response.json(result.rows);
+        })
+    })
 });
 
 module.exports = router;
