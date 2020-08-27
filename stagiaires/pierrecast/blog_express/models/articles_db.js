de1947272d12511ba122148a2f73c52fdf2682ab
pierre-cast <p_castronovo@hotmail.com>
2020-08-27 13:29:14 +0200

    WIP

--- stagiaires/pierrecast/blog_express/models/articles_db.js
+++ stagiaires/pierrecast/blog_express/models/articles_db.js
@@ -1,3 +1,17 @@
+const pool = require("../db/pool");
+
+async function getListArticles() {
+    return new Promise((resolve, reject) => {
+        await pool.query(``)
+        
+        if (isEven) {
+            resolve([1000, "Success"]);
+        } else {
+            reject([1000, "Error"]);
+        }
+    });
+}
+
 function getListArticles() {
 
 }
