a1d72c5f8cafb13320711b8d21206c433a8f709d
pierre-cast <p_castronovo@hotmail.com>
2020-08-27 16:17:12 +0200

    Articles OK

--- stagiaires/pierrecast/blog_express/routes/articles.js
+++ stagiaires/pierrecast/blog_express/routes/articles.js
@@ -1,24 +1,69 @@
 const { Router } = require('express');
 const pool = require('../db/pool');
 const router = Router();
-const { getListArticles } = require('../models/articles_db');
+const { getListArticles,
+    getArticleById, 
+    getArticleByAuteur,
+    getArticleByPage,
+    createArticle, 
+    updateArticle, 
+    deleteArticle } = require('../models/articles_db');
 
-router.get('', (request, response) => {
-    console.log('route');
-    getListArticles()
+router.get('/:id', (request, response) => {
+    // GET /articles/1
+    const id = parseInt(request.params.id);
+    getArticleById(id)
     .then(result => response.json(result))
     .catch(error => response.status(500).send('Erreur'));
+    
 });
 
+router.get('', (request, response) => {
+    if (request.query.auteur_id) {
+        // GET /articles?auteur_id=1
+        const id = parseInt(request.query.auteur_id);
+        getArticleByAuteur(id)
+        .then(result => response.json(result))
+        .catch(error => response.status(500).send('Erreur'));
+    } else if (request.query.page && request.query.par_page) {
+         //GET /articles?page=1&par_page=10
+         const page = parseInt(request.query.page);
+         const par_page = parseInt(request.query.par_page);
+         getArticleByPage(page, par_page)
+        .then(result => response.json(result))
+        .catch(error => response.status(500).send('Erreur'));
+    } else {
+        // GET /articles
+        getListArticles()
+        .then(result => response.json(result))
+        .catch(error => response.status(500).send('Erreur'));
+    }
+});
 
-/*
-GET /articles
-GET /articles/1
-GET /articles?auteur_id=1
-GET /articles?page=1&par_page=10
-POST /articles
-PUT /articles/1
-DELETE /articles/1
-*/
+router.post('', (request, response) => {
+   // POST /articles
+    createArticle(request.body)
+    .then(result => response.json(result))
+    .catch(error => response.status(500).send('Erreur'));
+    
+});
+
+router.put('/:id', (request, response) => {
+    // PUT /articles/1
+    const id = parseInt(request.params.id);
+    updateArticle(id, request.body)
+    .then(result => response.json(result))
+    .catch(error => response.status(500).send('Erreur'));
+
+});
+
+router.delete('/:id', (request, response) => {
+    // DELETE /articles/1
+    const id = parseInt(request.params.id);
+    deleteArticle(id)
+    .then(result => response.json(result))
+    .catch(error => response.status(500).send('Erreur'));
+
+});
 
 module.exports = router;
