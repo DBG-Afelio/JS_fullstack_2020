bc7220f0d760ed1986d064a05149a6e1f638b059
pierre-cast <p_castronovo@hotmail.com>
2020-08-28 11:38:22 +0200

    Censures OK

--- stagiaires/pierrecast/blog_express/routes/commentaires.js
+++ stagiaires/pierrecast/blog_express/routes/commentaires.js
@@ -8,30 +8,38 @@ const { getListCommentaires,
     updateCommentaire, 
     deleteCommentaire } = require('../models/commentaires_db');
 
+const { mask } = require('../models/censures_db');
+
 router.get('/:id', (request, response) => {
     // GET /commentaires/1
     const id = parseInt(request.params.id);
     getCommentaireById(id)
-    .then(result => response.json(result))
+    .then(result => response.json(mask(result)))
     .catch(error => response.status(500).send('Erreur'));
     
 });
 
-router.get('', (request, response) => {
+router.get('', async (request, response) => {
     if (request.query.article_id) {
-        // GET /articles?auteur_id=1
+        // GET /commentaires?article_id=1
         const id = parseInt(request.query.article_id);
+       
         getCommentairesByArticle(id)
+        .then(result => mask(result))
         .then(result => response.json(result))
-        .catch(error => response.status(500).send('Erreur'));
+        .catch(error => response.status(500).send('Erreur : ' + error));
     
     } else {
-       // GET /commentaires
-        getListCommentaires()
-        .then(result => response.json(result))
-        .catch(error => response.status(500).send('Erreur'));
+        // GET /commentaires
+        try {
+            const result = await getListCommentaires();
+            const mask_result = await mask(result);
+            response.json(mask_result);
+        } catch (error) {
+            console.log(error);
+            response.status(500).send('Erreur');
+        }
     }
-    
 });
 
 router.post('', (request, response) => {
