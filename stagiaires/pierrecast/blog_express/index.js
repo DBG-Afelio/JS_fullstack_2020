4a85af2f3df83214b387f06c39d32b0685ef7c50
pierre-cast <p_castronovo@hotmail.com>
2020-08-28 11:38:22 +0200

    Censures OK

--- stagiaires/pierrecast/blog_express/index.js
+++ stagiaires/pierrecast/blog_express/index.js
@@ -4,6 +4,7 @@ const auteurs = require('./routes/auteurs');
 const commentaires = require('./routes/commentaires');
 const categories = require('./routes/categories');
 const categories_articles = require('./routes/categories_articles');
+const censures = require('./routes/censures');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use('/auteurs', auteurs);
 app.use('/commentaires', commentaires);
 app.use('/categories', categories);
 app.use('/categories_articles', categories_articles);
+app.use('/censures', censures);
 
 app.use((err, req, res, next) => {
     res.send(err);
