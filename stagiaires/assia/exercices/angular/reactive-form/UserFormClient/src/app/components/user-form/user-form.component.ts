7476edde9a2c2218984677a9a0f9c7e14d0f6bb3
Aacyiah <arbox00@outlook.com>
2020-09-13 00:36:57 +0200

    -

--- stagiaires/assia/exercices/angular/reactive-form/UserFormClient/src/app/components/user-form/user-form.component.ts
+++ stagiaires/assia/exercices/angular/reactive-form/UserFormClient/src/app/components/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { User } from 'src/app/models/user/user.model';
 
 @Component({
@@ -21,7 +21,23 @@ export class UserFormComponent implements OnInit, OnDestroy {
   ];
   public genderList: string[] = ['Homme', 'Femme', 'Trans', 'Autre'];
   public loginList: string[] = ['login1', 'LOGIN2', 'LogIN3', '_login-4'];
-  
+  public limits = {
+    firstNameMin: 3,
+    lastNameMin : 3,
+    firstNameMax : 10,
+    lastNameMax : 10,
+    loginMin : 6,
+  };
+  public strOnlyPattern: string = '[a-zA-Z ]*';
+  public jobList: Array<any> = [
+    { id: 1, title: 'Developpeur FrontEnd', group: 'A' }, 
+    { id: 2, title: 'Developpeur BackEnd', group: 'A' },
+    { id: 3, title: 'Integrateur', group: 'A' },
+    { id: 4, title: 'UX/UI', group: 'A' }, 
+    { id: 5, title: 'Rien de tout cela', group: 'B' },
+  ];
+   
+
     constructor(
       // private userService: UserService,
       public formB: FormBuilder,
@@ -37,39 +53,57 @@ export class UserFormComponent implements OnInit, OnDestroy {
     ngOnDestroy(): void{
       // this.userSubscription.unsubscribe();
     }
-  
+
+    
   public initForm(): void {
       
     this.userForm = this.formB.group({
 
       lastName : new FormControl(
         this.currentUser.lName,
-        [Validators.required, Validators.minLength(3), Validators.maxLength(8)],
+        [
+          Validators.required, 
+          Validators.minLength(this.limits.lastNameMin), 
+          Validators.maxLength(this.limits.lastNameMax),
+          Validators.pattern(this.strOnlyPattern),
+        ],
       ),
 
       firstName : new FormControl(
         this.currentUser.fName,
-        [Validators.minLength(3), Validators.maxLength(8)],
+        [
+          Validators.minLength(this.limits.firstNameMin),
+          Validators.maxLength(this.limits.firstNameMax),
+          Validators.pattern(this.strOnlyPattern),
+        ],
       ),
 
       email: new FormControl(
         this.currentUser.email,
-        [Validators.required, Validators.email],
+        [
+          Validators.required,
+          Validators.email],
       ),
 
       nation: new FormControl(
         this.currentUser.nation,
-        [Validators.required],
+        [
+          Validators.required,
+        ],
       ),
 
       gender: new FormControl(
         this.currentUser.gender,
-        [Validators.required],
-       ),
+        [
+          Validators.required,
+        ],
+      ),
 
-
-
-
+      jobs: new FormArray(
+        [],
+        Validators.required,
+        this.jobsCustomValidator(),
+        ),
 
       // pwdGrp: this.formB.group({
       //   pwd: this.pwd.control(),
@@ -84,19 +118,41 @@ export class UserFormComponent implements OnInit, OnDestroy {
       // }),
         
     });
+    this.addJobsCheckBoxes();
+
   }
-  
-    public onSubmitForm(): void{
-      const userFormValue = this.userForm.value;
-      console.log(userFormValue);
-      // const newUser = new User(
-      //   userFormValue['lastName'],
-      //   userFormValue['firstName'],
-      //   userFormValue['email'],
-      //   userFormValue['nation'],
-        
-      // );
-      // this.userService.addUser(newUser);
-    }
+
+  public get jobsFormArray():FormArray{
+    return this.userForm.get('jobs') as FormArray;
   }
+  public onSubmitForm(): void {
+
+    const userFormValue = this.userForm.value;
+    const selectedJobs = userFormValue.jobs
+    .map( (checked: boolean, i: number) => checked ? this.jobList[i].title : null)
+    .filter( (val: any) => val !== null);
+
+    console.log(userFormValue);
+    // const newUser = new User(
+    //   userFormValue['lastName'],
+    //   userFormValue['firstName'],
+    //   userFormValue['email'],
+    //   userFormValue['nation'],
+    //   selectedJobs,
+      
+    // );
+    // this.userService.addUser(newUser);
+  }
+
+  private addJobsCheckBoxes():void{
+    this.jobList.forEach(() => this.jobsFormArray.push(new FormControl(false)));
+    console.log('jobsFormArray : ', this.jobsFormArray);
+  }
+
+  public jobsCustomValidator():any{
+
+  }
+
+
   
+}
