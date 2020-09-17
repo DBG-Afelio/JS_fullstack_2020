95f1dcfab61dff67d95baa4e431b87760d6faf9e
Aacyiah <arbox00@outlook.com>
2020-09-18 01:14:35 +0200

    -

--- stagiaires/assia/exercices/angular/reactive-form/UserFormClient/src/app/services/user.service.ts
+++ stagiaires/assia/exercices/angular/reactive-form/UserFormClient/src/app/services/user.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  public url = 'http://localhost:3005/stagiaires';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getAll(): Observable<User[]> {
+    return this.http.get<User[]>
+  }
+
 }
