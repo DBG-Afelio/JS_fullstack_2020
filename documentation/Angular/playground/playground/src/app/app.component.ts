f436709501c75fd04ca25dce637d8297439e8606
Denis Boogaerts Afelio <d.boogaerts@afelio.be>
2020-09-18 10:23:10 +0200

    directives et reactive form

--- documentation/Angular/playground/playground/src/app/app.component.ts
+++ documentation/Angular/playground/playground/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Observable, Subject } from 'rxjs';
-import { TestService } from './services/test.service';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,31 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mon application';
-  cpt = 0;
-  observable: Observable<number>;
-  sujetexterne: Subject<number>;
 
-  constructor(private testService: TestService) {
-    this.title = 'Nouveau titre';
-    this.observable = this.testService.getNombre();
-    this.observable.subscribe((nb) => {
-      console.log(nb);
-    });
-
-    this.sujetexterne = this.testService.subject;
-
-    this.sujetexterne.subscribe((nb) => {
-      console.log('sujet emis', nb);
-    });
-  }
-
-  updateTitle(oldtitle: string) {
-    this.title = `update ${oldtitle}`;
-  }
-
-  updateCpt() {
-    this.cpt = 100;
+  constructor() {
+    
   }
 
 }
--- documentation/Angular/playground/playground/src/app/app.module.ts
+++ documentation/Angular/playground/playground/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Component } from '@angular/core';
+import { NgModule, Component, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
@@ -7,19 +7,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './component/my-component/my-component.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { ImageLoaderDirective } from './directives/image-loader.directive';
+import { StructuralImageLoaderDirective } from './directives/structural-image-loader.directive';
 
 @NgModule({
    declarations: [
       AppComponent,
       MyComponentComponent,
-      SideBarComponent
+      SideBarComponent,
+      ImageLoaderDirective,
+      StructuralImageLoaderDirective
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       FormsModule
    ],
-   providers: [],
+   providers: [
+      {provide: LOCALE_ID, useValue: "fr-BE"}
+   ],
    bootstrap: [
       AppComponent
    ]
