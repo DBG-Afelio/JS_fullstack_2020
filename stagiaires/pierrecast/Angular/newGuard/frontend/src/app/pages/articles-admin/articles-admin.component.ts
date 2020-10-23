1e4f2ec028f8bc99e53224dea0d72e8ef5decc9e
pierre-cast <p_castronovo@hotmail.com>
2020-10-23 11:37:48 +0200

    -

--- stagiaires/pierrecast/Angular/newGuard/frontend/src/app/pages/articles-admin/articles-admin.component.ts
+++ stagiaires/pierrecast/Angular/newGuard/frontend/src/app/pages/articles-admin/articles-admin.component.ts
@@ -11,32 +11,34 @@ import { AuthService } from 'src/app/services/auth.service';
 export class ArticlesAdminComponent implements  OnInit {
 
   public listArticles: Article[];
-
-  
-
+  public page: number = 1;
+  public pageSize: number = 6;
+  public collectionSize: number;
   public currentUser : any;
+
   constructor(
-      private articleService: ArticleService, 
-      private authService: AuthService
-    ) {
-      
-      this.authService.getCurrentUser().subscribe(
-        (user: any) => {
-          this.currentUser = user;
-          
-        }
-      );
-      this.createListArticles();
+    private articleService: ArticleService, 
+    private authService: AuthService
+  ) {
+    this.authService.getCurrentUser().subscribe(
+      (user: any) => {
+        this.currentUser = user;
+        
+      }
+    );
+    this.createListArticles();
   }
 
   createListArticles() {
     if (this.currentUser.roles === 'ADMIN') {
       this.articleService.getList().subscribe(list => { 
-        this.listArticles = list;
+        this.listArticles = list.slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+        this.collectionSize = list.length;
       });
     } else if (this.currentUser.roles === 'AUTHOR') {
       this.articleService.getMyArticles().subscribe(list => { 
-        this.listArticles = list;
+        this.listArticles = list.slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+        this.collectionSize = list.length;
       });
     }
   }
