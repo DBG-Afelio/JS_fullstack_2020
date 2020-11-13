5c8e54aeccb411e0b7597fb435d55fee23445503
Fripon08 <62310112+Fripon08@users.noreply.github.com>
2020-11-13 11:30:32 +0100

    commit

--- documentation/Angular/rxjs-playground/src/app/components/timer/timer.component.ts
+++ documentation/Angular/rxjs-playground/src/app/components/timer/timer.component.ts
@@ -1,7 +1,8 @@
 import { Time } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject, Subscription, timer } from 'rxjs';
-import { map, takeUntil, tap } from 'rxjs/operators';
+import { interval, Observable, Subject, Subscription, timer } from 'rxjs';
+import { map, takeUntil,take, tap } from 'rxjs/operators';
+import { TimerService } from 'src/app/services/timer.service';
 
 @Component({
   selector: 'app-timer',
@@ -12,18 +13,45 @@ export class TimerComponent implements OnInit, OnDestroy {
 
   public destroy$ = new Subject();
 
-  public timer= timer(new Date(),100 ).pipe(
+  public chrono:Time;
+  public subscription: Subscription
+
+  public timer= interval(100).pipe(
     tap(tps => {
       console.log('timer emit:',tps);
-    })
+    }),
+    take(100)
   );
    
-  constructor() { }
+  constructor(public timeService:TimerService) { 
+    
+  }
 
   ngOnInit() {
   }
 
 
-  public ngOnDestroy() { }
+  public ngOnDestroy() {
+    this.stopChrono()
+  }
 
+  public startChrono(){
+    const date = new Date();
+    if (!this.subscription) {
+      this.subscription = this.timer.subscribe(
+        ()=> {
+          this.chrono = this.timeService.getTimeFromMilliseconds(this.timeService.getMillseconds(date, new Date()))
+          console.log('emit')
+        },
+        ()=> console.log('error'),
+        ()=> console.log('complete')
+      )
+    }
+  }
+
+  public stopChrono(){
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
